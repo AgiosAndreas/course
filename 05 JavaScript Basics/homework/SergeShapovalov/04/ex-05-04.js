1fbfd5539cc054f249ed0be3abe8787232ecd5c1
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-04-30 19:21:03 +0300

    Написал в одну строку и добавил вариант с минусовым значением count

--- 05 JavaScript Basics/homework/SergeShapovalov/04/ex-05-04.js
+++ 05 JavaScript Basics/homework/SergeShapovalov/04/ex-05-04.js
@@ -1,10 +1,5 @@
-"use strict";
-
 function factorial(count) {
-	if (count <= 0) {
-		return 1;
-	}
-	return count * factorial(count - 1);
+	return count <= 0 ? 1 : count * factorial(count - 1);
 }
 
 // Проверяем работу функции
@@ -12,5 +7,7 @@ alert(
 	"По заданию:\n" +
 	"\"5\" - \"" + factorial(5) + "\"\n" +
 	"\"0\" - \"" + factorial(0) + "\"\n" +
-	"\"11\" - \"" + factorial(11) + "\"\n"
+	"\"11\" - \"" + factorial(11) + "\"\n" +
+	"От себя:\n" +
+	"\"-2\" - \"" + factorial(-2) + "\"\n"
  );
