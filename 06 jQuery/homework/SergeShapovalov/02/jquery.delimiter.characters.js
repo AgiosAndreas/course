8029e5ff06d6935435c16b7eb6461b1c01d7d93f
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-05-22 11:30:32 +0300

    Сделал форму указания номера телефона с помощью регулярного выражения, передающегося в плагин.

--- 06 jQuery/homework/SergeShapovalov/02/jquery.delimiter.characters.js
+++
@@ -1,7 +0,0 @@
-(function( $ ) {
-	$.fn.delimiterCharacters = function() {
-		this.keypress(function(event) {
-			return (event.keyCode >= 48 && event.keyCode <= 57) || event.keyCode == 43;
-		});
-	};
-})(jQuery);
