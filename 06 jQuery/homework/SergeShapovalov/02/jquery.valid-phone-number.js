abab0ddc00383e25d03e25abeebea60c70d68566
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-05-25 20:51:20 +0300

    Отличная функция, спасибо. Поправил остальное.

--- 06 jQuery/homework/SergeShapovalov/02/jquery.valid-phone-number.js
+++ 06 jQuery/homework/SergeShapovalov/02/jquery.valid-phone-number.js
@@ -1,35 +1,29 @@
 (function( $ ) {
-	$.fn.validPhoneNumber = function(options, callback) {
+	$.fn.validPhoneNumber = function(options, callback, error) {
 
-		var DEFAULT_PATTERN = /^\d*$/;
-		var DEFAULT_MINCHARACTERS = 5;
-		var DEFAULT_MAXCHARACTERS = 20;
-
-		if (typeof options === "object") {
-			options.pattern = options.pattern || DEFAULT_PATTERN;
-			options.minCharacters = options.minCharacters || DEFAULT_MINCHARACTERS;
-			options.maxCharacters = options.maxCharacters || DEFAULT_MAXCHARACTERS;
-		} else {
-			options = {
-				pattern: DEFAULT_PATTERN,
-				minCharacters: DEFAULT_MINCHARACTERS,
-				maxCharacters: DEFAULT_MAXCHARACTERS
-			}
+		var validOptions = {
+			pattern: /^\d*$/,
+			minLength: 5,
+			maxLength: 20
 		}
+		$.extend(validOptions, options);
 
 		var oldValue = "";
 
 		this.on("input", function(event) {
 
-			if ($(this).val().length <= options.maxCharacters && ($(this).val().search(options.pattern) == 0 || $(this).val() == "")) {
+			var validLength =  $(this).val().length <= validOptions.maxLength;
+			if (validLength && ($(this).val() == "" || $(this).val().search(validOptions.pattern) == 0)) {
 				oldValue = $(this).val();
 			} else {
 				$(this).val(oldValue);
 			}
 
 			if ($.isFunction(callback)) {
-				var isValid = $(this).val().length >= options.minCharacters;
+				var isValid = $(this).val().length >= validOptions.minLength;
 				callback(isValid);
+			} else {
+				error("Параметр callback должен быть функцией");
 			}
 
 		});
