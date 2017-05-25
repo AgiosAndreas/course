fb8562d142d550c0e0ea4fe0fc469b55eebda860
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-05-25 10:54:13 +0300

    Переименовал объект параметров, задал значения по умолчанию и реализовал callback.

--- 06 jQuery/homework/SergeShapovalov/02/jquery.valid-phone-number.js
+++ 06 jQuery/homework/SergeShapovalov/02/jquery.valid-phone-number.js
@@ -1,28 +1,35 @@
 (function( $ ) {
-	$.fn.validPhoneNumber = function(pattern, blockParams) {
+	$.fn.validPhoneNumber = function(options, callback) {
+
+		var DEFAULT_PATTERN = /^\d*$/;
+		var DEFAULT_MINCHARACTERS = 5;
+		var DEFAULT_MAXCHARACTERS = 20;
+
+		if (typeof options === "object") {
+			options.pattern = options.pattern || DEFAULT_PATTERN;
+			options.minCharacters = options.minCharacters || DEFAULT_MINCHARACTERS;
+			options.maxCharacters = options.maxCharacters || DEFAULT_MAXCHARACTERS;
+		} else {
+			options = {
+				pattern: DEFAULT_PATTERN,
+				minCharacters: DEFAULT_MINCHARACTERS,
+				maxCharacters: DEFAULT_MAXCHARACTERS
+			}
+		}
+
 		var oldValue = "";
 
 		this.on("input", function(event) {
 
-			// Смотрим соответствие регулярному выражению
-			// Извини за длинную конструкцию, но ее сокращение выглядит еще хуже
-			if ($(this).val().length <= blockParams.maxCharacters && ($(this).val().search(pattern) == 0 || $(this).val() == "")) {
+			if ($(this).val().length <= options.maxCharacters && ($(this).val().search(options.pattern) == 0 || $(this).val() == "")) {
 				oldValue = $(this).val();
 			} else {
 				$(this).val(oldValue);
 			}
 
-			// Визуально блокируем/разблокируем кнопку при достижении количества необхоимых символов
-			if ($(this).val().length >= blockParams.minCharacters) {
-				$(blockParams.buttonName).css({
-					"background-color":blockParams.colorEnabled,
-					"cursor":"pointer"
-				})
-			} else {
-				$(blockParams.buttonName).css({
-					"background-color":blockParams.colorDisabled,
-					"cursor":"default"
-				})
+			if ($.isFunction(callback)) {
+				var isValid = $(this).val().length >= options.minCharacters;
+				callback(isValid);
 			}
 
 		});
