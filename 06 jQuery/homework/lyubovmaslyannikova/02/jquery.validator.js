d1acc91b154f942049bb7ca15bbbe473bb5cafed
lyubovmaslyannikova <lyubov.maslyannikova@mail.ru>
2017-06-09 20:27:01 +0300

    #6.2 Добавлен callBack

--- 06 jQuery/homework/lyubovmaslyannikova/02/jquery.validator.js
+++ 06 jQuery/homework/lyubovmaslyannikova/02/jquery.validator.js
@@ -6,25 +6,42 @@
 			pattern: /^(\+([0-9])*)$/,
 			minLength: 9,
 			maxLength: 17,
-			specialChar : "+"
+			specialChar : '+',
+			afterValidation: highlightInput
 		}, settings);
 
-		this.prop("maxlength", settings.maxLength);
+		this.prop('maxlength', settings.maxLength);
 
-		this.on("change keyup input", function() {
+		this.on('change keyup input', function() {
+
 			var phone = this.value;
 
 			if(!settings.pattern.test(phone)) {
-				phone = phone.length > 1 ? settings.specialChar + phone.replace(/\D/g, '') : "";
-
-				this.value = phone;
+				this.value = phone = phone.length > 1 ? settings.specialChar + phone.replace(/\D/g, '') : '';
 			}
 
-			var color = phone.length < settings.minLength ? "red" : "green";
-			$(this).css("border-color", color);
+			if(typeof settings.afterValidation === 'function') {
+
+				var data = {
+					minLength: settings.minLength
+				};
+
+				settings.afterValidation.call(this, data);
+			}
 		});
 
 		return this;
 	};
+
+	function highlightInput(data) {
+		if( !data ) {
+			return false;
+		}
+
+		var $this = $(this);
+		var color = $this.val().length < data.minLength ? 'red' : 'green';
+
+		$this.css('border-color', color);
+	}
 
 }(jQuery));
