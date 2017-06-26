212fd5afb48183801f6dabb8150cdc3c87275be0
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-06-26 22:59:27 +0300

    Вынес проверку параметров в отдельную функцию + остальные правки.

--- 06 jQuery/homework/SergeShapovalov/02/jquery.validate-phone.js
+++ 06 jQuery/homework/SergeShapovalov/02/jquery.validate-phone.js
@@ -1,53 +1,75 @@
+"use strict";
+
 (function( $ ) {
 	$.fn.validatePhone = function(options, phoneChange) {
 
-		switch(arguments.length) {
-		  case 0:
-				options = {};
-				phoneChange = function(){};
-		    break;
+		let checkedParams = checkParams(arguments.length, options, phoneChange);
+		options = checkedParams.options;
+		phoneChange = checkedParams.phoneChange;
 
-		  case 1:
-				if ($.isFunction(options)) {
-					phoneChange = options;
-					options = {};
-				} else if (!$.isPlainObject(options)) {
-					throw new TypeError("Передаваемый параметр метода должен быть функцией или объектом.");
-				}
-		    break;
-
-			case 2:
-				if (!$.isPlainObject(options) || !$.isFunction(phoneChange)) {
-					throw new TypeError("Первый параметр должен быть объектом, а второй функцией.");
-				}
-				break;
-
-		  default:
-				throw new TypeError("Метод принимает не более двух параметров: объект и функция");
-		}
-
-		var defaultOptions = {
-			pattern: /^\+[0-9]*$/,
-			minLength: 9,
-			maxLength: 18
-		}
-		options = $.extend({}, defaultOptions, options);
-
-		var oldValue = "";
+		let oldValue = "";
 
 		this.on("input", function(event) {
 
-			var inputText = $(this).val();
-			var validLength =  inputText.length <= options.maxLength;
+			let inputText = $(this).val();
+			let validPattern = inputText == "" || inputText.search(options.pattern) == 0;
+			let validLength =  inputText.length <= options.maxLength;
 
-			if (validLength && (inputText == "" || inputText.search(options.pattern) == 0)) {
+			if (validLength && validPattern) {
 				oldValue = inputText;
+				phoneChange(inputText.length >= options.minLength);
 			} else {
 				$(this).val(oldValue);
 			}
-
-			phoneChange(inputText.length >= options.minLength);
 		});
 
 	};
 })(jQuery);
+
+// -----------------------------------------------------------------------------
+
+function checkParams(countParams, options, phoneChange) {
+
+	let resultParams = {};
+
+	switch(countParams) {
+		case 0:
+			resultParams.options = {};
+			resultParams.phoneChange = function(){};
+			break;
+
+		case 1:
+			if ($.isFunction(options)) {
+				resultParams.phoneChange = options;
+				resultParams.options = {};
+			} else if ($.isPlainObject(options)) {
+				resultParams.options = options;
+				resultParams.phoneChange = function(){};
+			} else {
+				throw new TypeError("Передаваемый параметр метода должен быть функцией или объектом.");
+			}
+			break;
+
+		case 2:
+			if ($.isPlainObject(options) && $.isFunction(phoneChange)) {
+				resultParams.options = options;
+				resultParams.phoneChange = phoneChange;
+			} else {
+				throw new TypeError("Первый параметр должен быть объектом, а второй функцией.");
+			}
+			break;
+
+		default:
+			throw new TypeError("Метод принимает не более двух параметров: объект и функция");
+			break;
+	}
+
+	let defaultOptions = {
+		pattern: /^\+[0-9]*$/,
+		minLength: 9,
+		maxLength: 18
+	}
+	resultParams.options = $.extend({}, defaultOptions, resultParams.options);
+
+	return resultParams;
+}
