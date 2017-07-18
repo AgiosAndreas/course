94fe20b99fbc83de271aa4c889cb5b717afd6ab8
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-07-18 15:16:45 +0300

    ex-07-01. Подходит ли подобная реализация задания?

---
+++ 07 Node/homework/SergeShapovalov/01/ex-07-01.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function totalFlatten(array) {
+
+  if (array === null || typeof array !== "object") return null;
+  if (array.length === 0) return [];
+
+  let result = [];
+	for (let i = 0; i < array.length; i++) {
+
+    if (typeof array[i] === "object") {
+      result = result.concat(totalFlatten(array[i]));
+    } else {
+      result.push(array[i]);
+    }
+	}
+  return result;
+}
+
+console.log(totalFlatten(null));
+console.log(totalFlatten("null"));
+console.log(totalFlatten([]));
+console.log(totalFlatten([1,2,3]));
+console.log(totalFlatten([[1,2,3],["a","b","c"],[4,5,6]]));
+console.log(totalFlatten([[[1,2,3]]]));
