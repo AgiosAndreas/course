afdf34fc66d197f106ce8a2aa3a610830267fc08
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-07-28 17:05:01 +0300

    Константа и условие лишние, убрал. Описание поправил.

--- 07 Node/homework/SergeShapovalov/04/ex-07-04.js
+++ 07 Node/homework/SergeShapovalov/04/ex-07-04.js
@@ -7,16 +7,17 @@ let app = express();
 app.all("/echo", function (request, response) {
 
 	if (request.method == "POST") {
+
 		request.query.body = "";
 
 		request.on("data", function (data) {
 			request.query.body += data;
-			if (request.query.body.length > 1e6) request.connection.destroy();
 		});
 
 		request.on("end", function () {
 			response.send(JSON.stringify(request.query));
 		});
+
 	} else {
 		response.send(JSON.stringify(request.query));
 	}
