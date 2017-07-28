08f8644a7446d68430705a56a4907452a10ebc13
Sergey Shapovalov <projectv.scorpion@gmail.com>
2017-07-28 14:58:08 +0300

    Реализовано с помощью map, убрал константы из конструктора.

--- 07 Node/homework/SergeShapovalov/02/morse-decoder.js
+++ 07 Node/homework/SergeShapovalov/02/morse-decoder.js
@@ -1,76 +1,77 @@
 "use strict";
 
+const DELIM_LETTER  = "   ";
+const DELIM_WORD = "       ";
+const ABC = {
+	". -": "A",
+	"- . . .": "B",
+	"- . - .": "C",
+	"- . .": "D",
+	".": "E",
+	". . - .": "F",
+	"- - .": "G",
+	". . . .": "H",
+	". .": "I",
+	". - - -": "J",
+	"- . -": "K",
+	". - . .": "L",
+	"- -": "M",
+	"- .": "N",
+	"- - -": "O",
+	". - - .": "P",
+	"- - . -": "Q",
+	". - .": "R",
+	". . .": "S",
+	"-": "T",
+	". . -": "U",
+	". . . -": "V",
+	". - -": "W",
+	"- . . -": "X",
+	"- . - -": "Y",
+	"- - . .": "Z",
+	". - - - -": "1",
+	". . - - -": "2",
+	". . . - -": "3",
+	". . . . -": "4",
+	". . . . .": "5",
+	"- . . . .": "6",
+	"- - . . .": "7",
+	"- - - . .": "8",
+	"- - - - .": "9",
+	"- - - - -": "0"
+};
+
 class MorseDecoder {
-
-	constructor() {
-
-		MorseDecoder.DELIM_LETTER  = "   ";
-		MorseDecoder.DELIM_WORD = "       ";
-
-		MorseDecoder.ABC = {
-			". -": "A",
-			"- . . .": "B",
-			"- . - .": "C",
-			"- . .": "D",
-			".": "E",
-			". . - .": "F",
-			"- - .": "G",
-			". . . .": "H",
-			". .": "I",
-			". - - -": "J",
-			"- . -": "K",
-			". - . .": "L",
-			"- -": "M",
-			"- .": "N",
-			"- - -": "O",
-			". - - .": "P",
-			"- - . -": "Q",
-			". - .": "R",
-			". . .": "S",
-			"-": "T",
-			". . -": "U",
-			". . . -": "V",
-			". - -": "W",
-			"- . . -": "X",
-			"- . - -": "Y",
-			"- - . .": "Z",
-			". - - - -": "1",
-			". . - - -": "2",
-			". . . - -": "3",
-			". . . . -": "4",
-			". . . . .": "5",
-			"- . . . .": "6",
-			"- - . . .": "7",
-			"- - - . .": "8",
-			"- - - - .": "9",
-			"- - - - -": "0"
-		};
-	}
-
-	//----------------------------------------------------------------------------
 
 	decode(code) {
 
 		if (typeof code !== "string") return "";
+
 		code = code.trim();
 		if (code.length === 0) return "";
 
-		let words = code.split(MorseDecoder.DELIM_WORD);
-
 		let suggestion = "";
-		for (let i = 0; i < words.length; i++) {
+		let noLetter = false;
 
-			let word = words[i].split(MorseDecoder.DELIM_LETTER);
+		code.split(DELIM_WORD).map(function(word) {
 
-			for (let j = 0; j < word.length; j++) {
-				if (!(word[j] in MorseDecoder.ABC)) return "";
-				suggestion += MorseDecoder.ABC[word[j]];
-			}
+			word.split(DELIM_LETTER).map(function(letter) {
 
-			if (i < words.length - 1) suggestion += " ";
-		}
-		return suggestion;
+				if (letter in ABC) {
+					suggestion += ABC[letter];
+				} else {
+					noLetter = true;
+				}
+			});
+
+			suggestion += " ";
+		});
+
+		if (noLetter) return "";
+
+		return suggestion.slice(0, suggestion.length - 1);
 	}
+
 }
 
 module.exports = MorseDecoder;
