54d4423b328848476feb5d3739fd8d80627cc196
MirshaAndrey <mirsha.andrey@gmail.com>
2017-08-03 09:48:35 +0300

    правки ex-06-02
    переформатирование строки 15

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,24 +1,22 @@
-(function($)
-{
+(function($) {
 
-	$.fn.checkPhoneForm = function(options)
-	{
-		options = $.extend(
-		{
+	$.fn.checkform = function(options) {
+		options = $.extend({
 			minLength: 9,
 			maxLength: 17,
 			format: /^(\+([0-9])*)$/,
-			CharPlus: '+',
+			Charplus: '+',
 		}, options);
-		
+
 		this.prop('maxlength', options.maxLength);
-		this.on('change keyup input', function()
-		{
+		this.on('change keyup input', function() {
 			var phone = this.value;
 
-			if(options.format.test(phone)||(this.value=phone=1<phone.length?options.Charplus+phone.replace(/\D/g,''):''))
-			{
-				var data = { minLength: options.minLength };
+			if (options.format.test(phone) || (
+				this.value = phone = 1 < phone.length ? options.Charplus + phone.replace(/\D/g, '') : '')) {
+				var data = {
+					minLength: options.minLength
+				};
 			}
 
 		});
