bbd5be8277e25f31952f0393608c244d71d25e32
MirshaAndrey <mirsha.andrey@gmail.com>
2017-08-05 16:48:14 +0300

    правки ex-06-02

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,27 +1,22 @@
 (function($) {
 
-	$.fn.checkform = function(options) {
+	$.fn.checkPhoneForm = function(options) {
 		options = $.extend({
 			minLength: 9,
 			maxLength: 17,
 			format: /^(\+([0-9])*)$/,
-			Charplus: '+',
+			CharPlus: '+',
 		}, options);
 
 		this.prop('maxlength', options.maxLength);
 		this.on('change keyup input', function() {
 			var phone = this.value;
 
-			if (options.format.test(phone) || (
-				this.value = phone = 1 < phone.length ? options.Charplus + phone.replace(/\D/g, '') : '')) {
-				var data = {
-					minLength: options.minLength
-				};
+			if (options.format.test(phone) || (this.value = phone = 1 < phone.length ? options.Charplus + phone.replace(/\D/g, '') : '')) {
+				return this;
 			}
 
 		});
-
-		return this;
 	};
 
 }(jQuery));
