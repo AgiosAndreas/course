d5086bf0ef765537b4771459b8f661ed3b87557e
MirshaAndrey <mrisha.andrey@gmail.com>
2017-08-27 17:47:47 +0300

    правки ex-06-02

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,24 +1,26 @@
 (function($) {
 
-	$.fn.checkPhoneForm = function(options) {
-		options = $.extend({
-			minLength: 9,
-			maxLength: 17,
-			format: /^(\+([0-9])*)$/,
-			CharPlus: '+',
-		}, options);
+    $.fn.checkPhoneForm = function(options) {
+        options = $.extend({
+            minLength: 9,
+            maxLength: 17,
+            format: /^(\+([0-9])*)$/,
+            CharPlus: '+',
+        }, options);
 
-		this.prop('maxlength', options.maxLength);
-		this.on('change keyup input', function() {
-			var phone = this.value;
+        this.prop('maxlength', options.maxLength);
+        this.on('change keyup input', function() {
+            var phone = this.value;
 
-			if (options.format.test(phone) || (
-				this.value = phone = 1 < phone.length ? options.Charplus + phone.replace(/\D/g, '') : '')
-			   ) {
-				return this;
-			}
+            if (options.format.test(phone) || (
+                    this.value = phone = 1 < phone.length ? options.CharPlus + phone.replace(/\D/g, '') : ''))
 
-		});
-	};
-
+                var inpLength = 9;
+            if (inpLength < $('input').val().length) {
+                $('input').css("background-color", "#0cfa00");
+            } else {
+                $('input').css("background-color", "#ff3d3d");
+            }
+        });
+    };
 }(jQuery));
