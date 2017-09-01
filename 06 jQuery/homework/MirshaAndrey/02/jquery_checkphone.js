922cf81c16ae82d9deb008812b15539f3f7e7b7a
MirshaAndrey <mirsha.andrey@gmail.com>
2017-09-01 14:17:42 +0300

    правки ex-06-05

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,26 +1,26 @@
 (function($) {
 
-    $.fn.checkPhoneForm = function(options) {
-        options = $.extend({
-            minLength: 9,
-            maxLength: 17,
-            format: /^(\+([0-9])*)$/,
-            CharPlus: '+',
-        }, options);
+	$.fn.checkPhoneForm = function(options) {
+		options = $.extend({
+			minLength: 9,
+			maxLength: 17,
+			format: /^(\+([0-9])*)$/,
+			charPlus: '+',
+		}, options);
 
-        this.prop('maxlength', options.maxLength);
-        this.on('change keyup input', function() {
-            var phone = this.value;
+		this.prop('maxlength', options.maxLength);
+		this.on('change keyup input', function() {
+			var phone = this.value;
 
-            if (options.format.test(phone) || (
-                    this.value = phone = 1 < phone.length ? options.CharPlus + phone.replace(/\D/g, '') : ''))
+			if (0 < phone.length) {
+				this.value = options.charPlus + phone.replace(/\D/g, '');
+			} else {
+				this.value;
+			}
 
-                var inpLength = 9;
-            if (inpLength < $('input').val().length) {
-                $('input').css("background-color", "#0cfa00");
-            } else {
-                $('input').css("background-color", "#ff3d3d");
-            }
-        });
-    };
+			var color = options.minLength < this.value.length ? '#0cfa00' : '#ff3d3d';
+			$(this).css('background-color', color);
+		});
+	};
+
 }(jQuery));
