d99d2cd9254ae83c6c7ad49e204a8295333310e6
MirshaAndrey <mirsha.andrey@gmail.com>
2017-09-13 00:55:38 +0300

    правки ex-06-02
    правки форматирования

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -7,20 +7,17 @@
 			format: /^(\+([0-9])*)$/,
 			charPlus: '+',
 		}, options);
-
 		this.prop('maxlength', options.maxLength);
 		this.on('change keyup input', function() {
 			var phone = this.value;
-
 			if (0 < phone.length) {
 				this.value = options.charPlus + phone.replace(/\D/g, '');
 			} else {
 				this.value;
 			}
-
+			
 			var color = options.minLength < this.value.length ? '#0cfa00' : '#ff3d3d';
 			$(this).css('background-color', color);
 		});
 	};
-
 }(jQuery));
