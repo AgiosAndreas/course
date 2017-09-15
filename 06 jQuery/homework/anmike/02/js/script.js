c2c1f78251fb3e4d5f0cac8fe8dcd3794c2af491
anmike <anastaisha.mike@gmail.com>
2017-09-16 01:39:34 +0300

    Сменила логику 2 поля Андрей просил по заданию

--- 06 jQuery/homework/anmike/02/js/script.js
+++ 06 jQuery/homework/anmike/02/js/script.js
@@ -1,8 +1,30 @@
 "use strict";
 
 $(function() {
-  let phoneValid = $("#inputSuccess1").phoneValidation();
-  if (!phoneValid) {
-    $("#this").parents().removeClass(".has-success");
-  }
+  $("#inputSuccess1").phoneValidation({
+    onValidation(validPhone) {
+      $(this).css({
+        "color": "red",
+        "border-color": "red"
+      });
+      if (validPhone) {
+        $(this).css({
+          "color": "green",
+          "border-color": "green"
+        });
+      }
+    }
   });
+
+  $("#inputSuccess2").phoneValidation({
+    onValidation(validPhone) {
+      $("#inputSuccess2").parent().removeClass("has-success");
+      $("#inputSuccess2").siblings().removeClass("btn-info");
+      if (validPhone) {
+        $("#inputSuccess2").parent().addClass("has-success");
+        $("#inputSuccess2").siblings().addClass("btn-info");
+      }
+    }
+  });
+
+});
--- 06 jQuery/homework/anmike/02/js/validation.js
+++ 06 jQuery/homework/anmike/02/js/validation.js
@@ -5,65 +5,35 @@
   $.fn.phoneValidation = function(options) {
 
     let settings = $.extend({
+      onValidation: function() {},
       minLength: 8,
       maxLength: 17,
-      pattern: /^[0-9]+$/,
+      pattern: /^\+\d*$/,
       symbol: "+"
     }, options);
 
+    let currentValue = settings.symbol;
+    let validationChecker = 0;
 
-    function onKeyPress(e) {
-      if (((e.which < 48 || e.which > 57) && e.which != 43) || (this.value.length > settings.maxLength)) {
-        e.preventDefault();
-        //43 - +
+    this.on("input", function() {
+      let newValue = $(this).val();
 
-        //let validationChecker = 0;
-        //  if (phone.search(settings.pattern) == 0 ) {
-        //    validationChecker = 1
-        //  }
-        //  return validationChecker;
-      } else {
-        //let newText = String.fromCharCode(e.keyCode);
-
-      };
-    };
-
-    function onPaste(e) {
-
-      let pastedText = (e.originalEvent || e).clipboardData.getData('text/plain');
-      if (!settings.pattern.test(pastedText)) {
-        e.preventDefault();
-      } else {
-
-        //var presentText = $(this).val();
-
-        let presentText = $(this).val();
-        let freeSpace = settings.maxLength - presentText.length;
-        alert(freeSpace);
-        if (freeSpace) {
-          pastedText = pastedText.slice(0, freeSpace)
-            (e.originalEvent || e).clipboardData.setData('text/plain', pastedText);
-        } else {
-          e.preventDefault();
+      if (settings.pattern.test(newValue) && newValue.length < settings.maxLength) {
+        validationChecker = 0;
+        currentValue = newValue;
+        $(this).val(newValue);
+        if (newValue.length > settings.minLength) {
+          validationChecker = 1;
         }
-        //  let finalText = presentText + pastedText;
-        //  point = $(this).selectionStart;
-        //  alert (point);
-        //  alert(presentText + pastedText);
-        //  alert (settings.pattern.test(presentText + pastedText));
-        //  var result = $(this).val(presentText.slice(0,point) + pastedText + presentText.slice(point));
-        //  alert(result);
-
-        //    $(this).val(presentText.slice(0 , settings.maxLength));
-
+      } else {
+        $(this).val(currentValue);
       }
 
-      //  checkInput($(this));
-      //  $(this).val(presentText + pastedText);
-    };
-
-    this.val(settings.symbol);
-    this.bind('keypress', onKeyPress).bind('paste', onPaste);
+      if ($.isFunction(settings.onValidation)) {
+        settings.onValidation.call(this, validationChecker);
+      }
+    });
+    return this;
   };
 
 }(jQuery));
