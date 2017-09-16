41b9bd69ca557d5f5d38c417ad5a89e2b03053e8
anmike <anastaisha.mike@gmail.com>
2017-09-16 10:01:37 +0300

    Косметические изменения

--- 06 jQuery/homework/anmike/02/js/script.js
+++ 06 jQuery/homework/anmike/02/js/script.js
@@ -1,30 +1,29 @@
 "use strict";
 
 $(function() {
-  $("#inputSuccess1").phoneValidation({
-    onValidation(validPhone) {
-      $(this).css({
-        "color": "red",
-        "border-color": "red"
-      });
-      if (validPhone) {
-        $(this).css({
-          "color": "green",
-          "border-color": "green"
-        });
-      }
-    }
-  });
+	$("#inputSuccess1").phoneValidation({
+		onValidation(validPhone) {
+			$(this).css({
+				"color": "red",
+				"border-color": "red"
+			});
+			if (validPhone) {
+				$(this).css({
+					"color": "green",
+					"border-color": "green"
+				});
+			}
+		}
+	});
 
-  $("#inputSuccess2").phoneValidation({
-    onValidation(validPhone) {
-      $("#inputSuccess2").parent().removeClass("has-success");
-      $("#inputSuccess2").siblings().removeClass("btn-info");
-      if (validPhone) {
-        $("#inputSuccess2").parent().addClass("has-success");
-        $("#inputSuccess2").siblings().addClass("btn-info");
-      }
-    }
-  });
-
+	$("#inputSuccess2").phoneValidation({
+		onValidation(validPhone) {
+			$(this).parent().removeClass("has-success");
+			$(this).siblings().removeClass("btn-info");
+			if (validPhone) {
+				$(this).parent().addClass("has-success");
+				$(this).siblings().addClass("btn-info");
+			}
+		}
+	});
 });
--- 06 jQuery/homework/anmike/02/js/validation.js
+++ 06 jQuery/homework/anmike/02/js/validation.js
@@ -12,28 +12,28 @@
       symbol: "+"
     }, options);
 
-    let currentValue = settings.symbol;
-    let validationChecker = 0;
+    return this.each(function() {
+      let $this = $(this);
+      let currentValue = settings.symbol;
+      let validationChecker = 0;
 
-    this.on("input", function() {
-      let newValue = $(this).val();
-
-      if (settings.pattern.test(newValue) && newValue.length < settings.maxLength) {
-        validationChecker = 0;
-        currentValue = newValue;
-        $(this).val(newValue);
-        if (newValue.length > settings.minLength) {
-          validationChecker = 1;
+      $this.on("input", function() {
+        let newValue = $this.val();
+        if (settings.pattern.test(newValue) && newValue.length <= settings.maxLength) {
+          validationChecker = 0;
+          currentValue = newValue;
+          $this.val(newValue);
+          if (newValue.length > settings.minLength) {
+            validationChecker = 1;
+          }
+        } else {
+          $this.val(currentValue);
         }
-      } else {
-        $(this).val(currentValue);
-      }
-
-      if ($.isFunction(settings.onValidation)) {
-        settings.onValidation.call(this, validationChecker);
-      }
+        if ($.isFunction(settings.onValidation)) {
+          settings.onValidation.call(this, validationChecker);
+        }
+      });
     });
-    return this;
   };
 
 }(jQuery));
