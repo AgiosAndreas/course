020515d3b49a2f0047a201e5db1fa0b614604579
MirshaAndrey <mirsha.andrey@gmail.com>
2017-09-17 17:23:44 +0300

    правки ex-06-02

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,23 +1,42 @@
 (function($) {
 
+	function checkinput(color) {
+
+		var check = color ? '#0cfa00' : '#ff3d3d';
+		$(this).css('background-color', check);
+
+	}
+
+	checkinput2 = function(color) {
+		var check = color ? '#0cfa00' : '#ff3d3d';
+		$(this).css('border-color', check);
+	}
+
 	$.fn.checkPhoneForm = function(options) {
 		options = $.extend({
 			minLength: 9,
 			maxLength: 17,
 			format: /^(\+([0-9])*)$/,
 			charPlus: '+',
+			highlightingCorrectness: checkinput
 		}, options);
+
 		this.prop('maxlength', options.maxLength);
 		this.on('change keyup input', function() {
 			var phone = this.value;
+
 			if (0 < phone.length) {
 				this.value = options.charPlus + phone.replace(/\D/g, '');
 			} else {
 				this.value;
 			}
-			
-			var color = options.minLength < this.value.length ? '#0cfa00' : '#ff3d3d';
-			$(this).css('background-color', color);
+
+			if (typeof options.highlightingCorrectness === 'function') {
+				var color = options.minLength < this.value.length;
+				options.highlightingCorrectness.call(this, color);
+			}
 		});
 	};
+
+
 }(jQuery));
