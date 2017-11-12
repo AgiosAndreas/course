b3731f9a1be53e77e860a2d38e8166a71f556182
MirshaAndrey <mirsha.andrey@gmail.com>
2017-11-12 17:01:11 +0300

    правки ex-07-01
    форматирование строка 9-14

--- 07 Node/homework/MirshaAndrey/01/index.js
+++ 07 Node/homework/MirshaAndrey/01/index.js
@@ -10,13 +10,5 @@ console.log(flatten(null));
 console.log(flatten('null'));
 console.log(flatten([]));
 console.log(flatten([1, 2, 3]));
-console.log(flatten([
-    [1, 2, 3],
-    ["a", "b", "c"],
-    [4, 5, 6]
-]));
-console.log(flatten([
-    [
-        [1, 2, 3]
-    ]
-]));
+console.log(flatten([[1, 2, 3], ["a", "b", "c"], [4, 5, 6]]));
+console.log(flatten([[[1, 2, 3]]]));
