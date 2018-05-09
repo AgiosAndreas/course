99233ff3637ff44aefc7ebd093c8a6fb7fead210
MirshaAndrey <mirsha.andrey@gmail.com>
2018-05-10 02:06:27 +0300

    ex-07-01
    проверка кода codacy и форматирование согласно её замечаниям

--- 07 Node/homework/MirshaAndrey/01/index.js
+++ 07 Node/homework/MirshaAndrey/01/index.js
@@ -1,13 +1,14 @@
-"use strict";
+'use strict';
 
 function flatten(data) {
+    
     return Array.isArray(data) ? data.reduce((result, array) => {
         return result.concat(array)}, []) : data;
 }
 
-Console.log(flatten(null));
-Console.log(flatten('null'));
-Console.log(flatten([]));
-Console.log(flatten([1, 2, 3]));
-Console.log(flatten([[1, 2, 3], ["a", "b", "c"], [4, 5, 6]]));
-Console.log(flatten([[[1, 2, 3]]]));
+console.log(flatten(null));
+console.log(flatten('null'));
+console.log(flatten([]));
+console.log(flatten([1, 2, 3]));
+console.log(flatten([[1, 2, 3], ["a", "b", "c"], [4, 5, 6]]));
+console.log(flatten([[[1, 2, 3]]]));
