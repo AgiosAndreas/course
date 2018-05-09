b6b6b739083101cd959fbb07f403c22a548d3d38
MirshaAndrey <mirsha.andrey@gmail.com>
2018-05-10 02:19:09 +0300

    ex-07-01
    смена кавычек с одинарных на двойные

--- 07 Node/homework/MirshaAndrey/01/index.js
+++ 07 Node/homework/MirshaAndrey/01/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function flatten(data) {
     
@@ -7,7 +7,7 @@ function flatten(data) {
 }
 
 console.log(flatten(null));
-console.log(flatten('null'));
+console.log(flatten("null"));
 console.log(flatten([]));
 console.log(flatten([1, 2, 3]));
 console.log(flatten([[1, 2, 3], ["a", "b", "c"], [4, 5, 6]]));
