99457f37e2b7924f47e79e34f624bbe20926fc8a
MirshaAndrey <mirsha.andrey@gmail.com>
2018-06-09 13:28:53 +0300

    ex-07-01

--- 07 Node/homework/MirshaAndrey/01/index.js
+++ 07 Node/homework/MirshaAndrey/01/index.js
@@ -1,17 +1,23 @@
 "use strict";
 
 function flatten(data) {
-    
+
     return Array.isArray(data) ? data.reduce((result, array) => {
-        return result.concat(array);
+    return result.concat(array);
     }, []) : data;
 }
 
 function totalFlatten(data) {
-
-    if (Array.isArray(data)) {
-        return [].concat(flatten(data));
-    }
+   
+    if (!Array.isArray(data)) {
+        return data;
+    } 
+    else {
+    	var newarr = [];
+        while (data.some(Array.isArray)) {
+            data = newarr.concat(flatten(data));
+        }
+    } 
     return data;
 }
 
@@ -21,3 +27,10 @@ console.log(flatten([]));
 console.log(flatten([1,2,3]));
 console.log(flatten([[1,2,3],["a","b","c"],[4,5,6]]));
 console.log(flatten([[[1,2,3]]]));
+
+//test for totalFlatten
+console.log(totalFlatten(null));
+console.log(totalFlatten('null'));
+console.log(totalFlatten([]));
+console.log(totalFlatten([[1,2,3],["a","b","c"], [4,5,6]]));
+console.log(totalFlatten([[[1,2,3]]]));
