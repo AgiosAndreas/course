33e02a8701c57a504d54023c033e55cd2d0393e9
MirshaAndrey <mirsha.andrey@gmail.com>
2018-06-13 16:00:00 +0300

    ex-06-02
    смена названия метода с checkPhoneForm на validInput
    замена с onValidation afterValidation
    замена colorResult на validShow
    убрал свойство с одинакомым именем

--- 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
+++ 06 jQuery/homework/MirshaAndrey/02/jquery_checkphone.js
@@ -1,12 +1,11 @@
 (function($) {
 
-	$.fn.checkPhoneForm = function(options) {
+	$.fn.validInput = function(options) {
 		options = $.extend({
 			minLength: 9,
 			maxLength: 17,
 			charPlus: '+',
-			onValidation: function() {},
-			onValidation: checkinput
+			afterValidation: checkinput
 		}, options);
 
 		this.prop('maxlength', options.maxLength);
@@ -17,9 +16,9 @@
 				this.value = options.charPlus + phone.replace(/\D/g, '');
 			}
 
-			if ($.isFunction(options.onValidation) == true) {
-				var colorResult = options.minLength < this.value.length;
-				options.onValidation.call(this, colorResult);
+			if ($.isFunction(options.afterValidation) == true) {
+				var validShow = options.minLength < this.value.length;
+				options.afterValidation.call(this, validShow);
 			}
 
 		});
