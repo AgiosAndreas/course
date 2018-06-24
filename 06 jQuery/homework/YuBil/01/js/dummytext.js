d30ac3e39bc0b7edee22b7f1bc7d8a4e0297f0d1
Yurii Bilyk <xdream.best@gmail.com>
2018-06-24 18:33:03 +0300

    Move bacon function to external file;

---
+++ 06 jQuery/homework/YuBil/01/js/dummytext.js
@@ -0,0 +1,14 @@
+function getDummyText() {
+    $.getJSON('https://baconipsum.com/api/?callback=?', 
+        { 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'1' }, 
+        function(baconGoodness)
+        {
+            if (baconGoodness && baconGoodness.length > 0)
+            {
+                var dummyTextSubString = baconGoodness[0].substr(0, $( "#paragraphLength" ).val());
+                $("#baconIpsumOutput").html('');
+                $("#baconIpsumOutput").append('<p>' + dummyTextSubString + '</p>');
+                $("#baconIpsumOutput").show();
+            }
+        });
+}
