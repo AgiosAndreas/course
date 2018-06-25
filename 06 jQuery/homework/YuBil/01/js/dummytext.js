f70ca22679e0a77cf55e027a6e19f5b1b599353a
Yurii Bilyk <xdream.best@gmail.com>
2018-06-25 14:42:17 +0300

    BS stylization ready;

--- 06 jQuery/homework/YuBil/01/js/dummytext.js
+++ 06 jQuery/homework/YuBil/01/js/dummytext.js
@@ -1,14 +1,13 @@
 function getDummyText() {
+
     $.getJSON('https://baconipsum.com/api/?callback=?', 
         { 'type':'meat-and-filler', 'start-with-lorem':'1', 'paras':'1' }, 
-        function(baconGoodness)
-        {
-            if (baconGoodness && baconGoodness.length > 0)
-            {
+        function(baconGoodness) {
+
+            if (baconGoodness && baconGoodness.length > 0) {
+                
                 var dummyTextSubString = baconGoodness[0].substr(0, $( "#paragraphLength" ).val());
-                $("#baconIpsumOutput").html('');
-                $("#baconIpsumOutput").append('<p>' + dummyTextSubString + '</p>');
-                $("#baconIpsumOutput").show();
+                $("#baconIpsumOutput").append('<p class="border-bottom border-gray">' + dummyTextSubString + '</p>');
             }
         });
 }
--- 06 jQuery/homework/YuBil/01/js/sortparagraphs.js
+++ 06 jQuery/homework/YuBil/01/js/sortparagraphs.js
@@ -1,12 +1,13 @@
 function sortParagraphs() {
+    
     var $elements = $( "p" );
-    console.log($elements);
     var $target = $( "#baconIpsumOutput" );
-    console.log($target); 
 
     $elements.sort(function (a, b) {
+
         return $(a).html().length - $(b).html().length;
+
     });
-     
+    
     $elements.detach().appendTo($target);
 }
