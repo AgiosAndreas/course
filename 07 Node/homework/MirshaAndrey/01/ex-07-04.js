973af67c4c46fe017e725b4db2c63efc5b670e8a
MirshaAndrey <mirsha.andrey@gmail.com>
2018-07-05 18:07:32 +0300

    ex-07-04

---
+++ 07 Node/homework/MirshaAndrey/01/ex-07-04.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+const bodyParser = require('body-parser');
+const express = require("express");
+const port = 8080;
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.listen(port, () => {
+	console.log('Server running on ' + port + ' port');
+});
+
+
+app.all("/echo", function (request, response) {
+	let data = request.method === "POST" ? request.body : request.query;
+	response.setHeader('Content-Type', 'application/json');
+	response.send(JSON.stringify(data, null, 2));
+});
