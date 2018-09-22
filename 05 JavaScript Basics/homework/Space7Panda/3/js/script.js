9f697a957dddf9d28751a310a25a7b50d7cac857
Space7Panda <space7panda@gmail.com>
2018-09-22 21:24:19 +0300

    fix syntax

--- 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
@@ -1,19 +1,20 @@
 "use strict";
 let ndigit = (number, index) => {
+	
 	if (isNaN(number) === true ||
 		isNaN(index)  === true ||
 		Number.isInteger(number) === false ||
 		Number.isInteger(index)  === false )
 	{
 		return;
-	}
+	};
 
 	number = Math.abs(number);
 	let divider = Math.pow(10, index - 1);
 
 	if (index < 1 || number < divider) {
 		return -1;
-	}
+	};
 
 	return (number / divider | 0) % 10;
 };
