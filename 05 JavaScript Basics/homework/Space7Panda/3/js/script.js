a16b346de5083669998ed97d9640a7fbaa4216a8
mgg <space7panda@gmail.com>
2018-09-23 09:57:51 +0300

    edit syntax

--- 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
@@ -7,14 +7,14 @@ let ndigit = (number, index) => {
 		Number.isInteger(index)  === false )
 	{
 		return;
-	};
+	}
 
 	number = Math.abs(number);
 	let divider = Math.pow(10, index - 1);
 
 	if (index < 1 || number < divider) {
 		return -1;
-	};
+	}
 
 	return (number / divider | 0) % 10;
 };
