e4d4a8214ff0654e53cef7a63722b4dfc1233399
Space7Panda <space7panda@gmail.com>
2018-09-26 17:29:10 +0300

    Выполнил задание

---
+++ 06 jQuery/homework/Space7Panda/1/js/sort.js
@@ -0,0 +1,13 @@
+function paragraphSort () {
+	var p = $("p");
+
+	p.sort(function(p1, p2) {
+		return $(p1).text().length - $(p2).text().length;
+	});
+
+	$('body').append(p);
+}
+
+$(document).ready(function () {
+	$('.sortButton').click(paragraphSort);
+});
