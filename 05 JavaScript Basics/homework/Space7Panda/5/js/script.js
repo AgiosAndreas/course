5069bcea266819ff41cd9088ebf885f5285e24a7
Space7Panda <space7panda@gmail.com>
2018-10-04 19:42:13 +0300

    undo

--- 05 JavaScript Basics/homework/Space7Panda/5/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/5/js/script.js
@@ -5,11 +5,20 @@ let sum = (a, b) => {
 
 	let addon = 0;
 	let result = "";
-	
-	for (let i = 1; i <= a.length || i <= b.length; i++) {		
+
+	for (let i = 1; i <= a.length || i <= b.length; i++) {
+			
 		let aNum = (i <= a.length) ? parseInt(a[a.length - i], 10) : 0;
 		let bNum = (i <= b.length) ? parseInt(b[b.length - i], 10) : 0;
 		let sum = aNum + bNum + addon;
+
+		if (sum > 9) {
+			addon = 1;
+			sum -= 10;
+		} else {
+			addon = 0;
+		}
+
 		result = sum + result;
 	}
 
