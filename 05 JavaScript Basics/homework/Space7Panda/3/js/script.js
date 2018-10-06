1960f84a985a785b9187335a3b4a8f30cbc57068
mgg <space7panda@gmail.com>
2018-10-06 13:57:31 +0300

    add try&catch

--- 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/3/js/script.js
@@ -1,18 +1,27 @@
 "use strict";
 function ndigit(number, index) {
+	try {
+
+		if (Number.isInteger(number) === false ||
+			Number.isInteger(index)  === false ) {
+			
+			throw {"Message":"value is not integer"};
+
+		}
 	
-	if (Number.isInteger(number) === false ||
-		Number.isInteger(index)  === false )
-	{
+		number = Math.abs(number);
+		let divider = Math.pow(10, index - 1);
+
+		if (index < 1 || number < divider) {
+			return -1;
+		}
+
+		return (number / divider | 0) % 10;
+
+	} catch (e) {
+
+		console.log("Error: " + e.Message);
 		return;
+
 	}
-
-	number = Math.abs(number);
-	let divider = Math.pow(10, index - 1);
-
-	if (index < 1 || number < divider) {
-		return -1;
-	}
-
-	return (number / divider | 0) % 10;
 };
