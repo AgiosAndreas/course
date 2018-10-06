6b21ca2a9656caaf0a39386bec81e962f78e2b84
mgg <space7panda@gmail.com>
2018-10-06 11:56:10 +0300

    edit throw

--- 05 JavaScript Basics/homework/Space7Panda/4/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/4/js/script.js
@@ -3,7 +3,7 @@ function factorial(n) {
 	try {
 		
 		if (Number.isInteger(n) === false || n < 0) {
-			throw {"Message": "Value is not integer"};
+			throw {"Message": "'n' - is not integer or 'n < 0'"};
 		}
 	
 		return (n <= 1) ? 1 : n * factorial(n - 1);
