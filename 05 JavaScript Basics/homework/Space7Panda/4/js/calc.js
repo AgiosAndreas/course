a92240def2ef8e5c88a9b8344e65dc152ae4111b
mgg <space7panda@gmail.com>
2018-10-06 11:44:48 +0300

    add try&catch

--- 05 JavaScript Basics/homework/Space7Panda/4/js/calc.js
+++ 05 JavaScript Basics/homework/Space7Panda/4/js/calc.js
@@ -3,7 +3,8 @@ function startCalc() {
 	let userValue = +prompt('Введите значение', 0);
 	
 	if (factorial(userValue) === undefined) {
-		return alert('Пожалуйста, укажите целое позитивное число');
+		alert('Пожалуйста, укажите целое позитивное число');
+		return;
 	}
 
 	alert(`Факториал ${userValue}! равен: ${factorial(userValue)}`);
