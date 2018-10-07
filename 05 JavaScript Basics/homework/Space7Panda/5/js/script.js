6d046e123c72e4938eb938dc5bdf41ba08e8e7b3
Space7Panda <space7panda@gmail.com>
2018-10-07 11:26:06 +0300

    Добавил проверку на число и исключение

--- 05 JavaScript Basics/homework/Space7Panda/5/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/5/js/script.js
@@ -1,26 +1,35 @@
 "use strict";
 let sum = (a, b) => {
-	a = a.replace(/[^0-9]/gim, "");
-	b = b.replace(/[^0-9]/gim, "");
+	try {
 
-	let addon = 0;
-	let result = "";
+		if(!a.match(/^\d+$/) || !b.match(/^\d+$/)) {
+			throw {"Message":"'a' or 'b' is not a number"};
+		} 
 
-	for (let i = 1; i <= a.length || i <= b.length; i++) {
-			
-		let aNum = (i <= a.length) ? parseInt(a[a.length - i], 10) : 0;
-		let bNum = (i <= b.length) ? parseInt(b[b.length - i], 10) : 0;
-		let sum = aNum + bNum + addon;
+		let addon = 0;
+		let result = "";
 
-		if (sum > 9) {
-			addon = 1;
-			sum -= 10;
-		} else {
-			addon = 0;
+		for (let i = 1; i <= a.length || i <= b.length; i++) {
+
+			let aNum = (i <= a.length) ? parseInt(a[a.length - i], 10) : 0;
+			let bNum = (i <= b.length) ? parseInt(b[b.length - i], 10) : 0;
+			let sum = aNum + bNum + addon;
+
+			if (sum > 9) {
+				addon = 1;
+				sum -= 10;
+			} else {
+				addon = 0;
+			}
+
+			result = sum + result;
 		}
 
-		result = sum + result;
-	}
+		return result;
+	
+	} catch(e) {
 
-	return result;
+		console.log("Error: " + e.Message);
+		return;
+	}
 };
--- 05 JavaScript Basics/homework/Space7Panda/5/js/sumCustom.js
+++ 05 JavaScript Basics/homework/Space7Panda/5/js/sumCustom.js
@@ -3,5 +3,11 @@ let startCustomSum = () => {
 	let a = prompt('Укажите "а"\nПробелы и спец. символы не учитываются', '3');
 	let b = prompt('Укажите "b"', '5');
 
-	return sum(a, b);
+	if (sum(a, b) === undefined ) {
+		alert('Ошибка: Пожалуйста введите целые числа.');
+		return;
+	}
+
+	 alert(sum(a, b));
+	 return;
 };
