7f9353f77cc1f0960f91fe045f37dd21801a583c
Space7Panda <space7panda@gmail.com>
2018-10-11 18:27:45 +0300

    заменил let на function

--- 05 JavaScript Basics/homework/Space7Panda/2/js/customSort.js
+++ 05 JavaScript Basics/homework/Space7Panda/2/js/customSort.js
@@ -1,11 +1,11 @@
 "use strict";
-let startCustomSort = () => {
+function startCustomSort() {
 	let value = prompt('Укажите числа через пробел.', '1 3 2');
 
 	if (sort(value) == undefined) {
 		alert('Ошибка: пожалуйста укажите целые числа');
 		return;
 	}
-	
+
 	alert(`[${sort(value)}]`);
 };
