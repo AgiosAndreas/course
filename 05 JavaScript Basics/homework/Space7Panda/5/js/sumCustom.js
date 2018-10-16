053fe0ba38a14ab8b01aed52b423904a1f7c53ba
Space7Panda <space7panda@gmail.com>
2018-10-16 16:39:19 +0300

    исправил обработку исключений

--- 05 JavaScript Basics/homework/Space7Panda/5/js/sumCustom.js
+++ 05 JavaScript Basics/homework/Space7Panda/5/js/sumCustom.js
@@ -4,15 +4,8 @@ function startCustomSum() {
 	let b = prompt('Укажите "b"', '5');
 
 	try {
-
-		if (sum(a, b) === undefined ) {
-			throw {"Error":"value is not a number"};
-		}
-
-	 	alert(sum(a, b));
+		alert(sum(a, b));
 	} catch (e) {
-		console.log("Finish with error " + e.Error);
-		
-		alert('Ошибка: Пожалуйста введите целые числа.');
+		alert("Ошибка: " + e.Error + "." + "\nПожалуйста введите целые числа.");
 	}
 };
