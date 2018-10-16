78e94dbb1f4f08845902a9672bdce38835d46ae4
Space7Panda <space7panda@gmail.com>
2018-10-16 22:04:44 +0300

    подключил ещё более простой плагин

--- 06 jQuery/homework/Space7Panda/3/js/script.js
+++ 06 jQuery/homework/Space7Panda/3/js/script.js
@@ -1,50 +1,17 @@
-$(function(){
-  $('.thumbnails a').click(function(){                                 // При нажатиина миниатюру
-    var images = $(this).find('img');
-    var imgSrc = images.attr('src');
- 
-    $(".big-image img").attr({ src: imgSrc });                         // Подменяем адрес большого изображения на адрес выбранного
-    $(this).siblings('a').removeClass('active');                       // Удаляем класс .active со ссылки чтоб убрать рамку
-    images.parent().addClass('active');                                // Добавляем класс .active на выбранную миниатюру
-    return false;
-  });
- 
-  $('.next').click(function(){                                         // При нажатии на кнопку "вперед"
-    var count = $('.thumbnails a').length;                             // Общее количество изображений
-    var n = parseInt($('.thumbnails a').index($('.active')) + 1);      // Порядковый номер текущего изображения
-    var activeImg = $('.thumbnails .active');                          // Активное на данный момент изображение
-    var nextSrc;
- 
-    if (count != n){                                                   // - Если изображение не последнее
-      nextSrc = activeImg.next().find('img').attr('src');              // В переменную записывается адрес следующего изображения
-      $('.thumbnails .active').removeClass('active');                  // Удаляется класс .active с предыдущей миниатюры
-      activeImg.next().addClass('active');                             // На миниатюру следующего изображения вешается класс .active
-    }else{                                                             // - Если текущее изображение последнее в списке
-      nextSrc = $('.thumbnails a').first().find('img').attr('src');    // В переменную записывается адрес первого изображения
-      $('.thumbnails .active').removeClass('active');                  // Удаляется класс .active с предыдущей миниатюры
-      $('.thumbnails a').first().addClass('active');                   // На первую миниатюру вешается класс .active
-    }
-    $('.big-image img').attr({ src: nextSrc });                        // Подменяем адрес большого изображения на адрес следующего
-    return false;
-  });
- 
- 
-  $('.prev').click(function(){                                         // При нажатии на кнопку "назад"
-    var count = $('.thumbnails a').length;                             // Общее количество изображений
-    var n = parseInt($('.thumbnails a').index($('.active')) + 1);      // Порядковый номер текущего изображения
-    var activeImg = $('.thumbnails .active');                          // Активное на данный момент изображение
-    var prevSrc;
- 
-    if (n != 1){                                                       // - Если текущее изображение не первое
-      prevSrc = activeImg.prev().find('img').attr('src');              // В переменную записывается адрес предыдущего изображения           
-      $('.thumbnails .active').removeClass('active');                  // Удаляется класс .active активной до этого миниатюры
-      activeImg.prev().addClass('active');                             // На миниатюру изображения слева вешается класс .active
-    }else{                                                             // - Если текущее изображение первое
-      prevSrc = $('.thumbnails a:last').find('img').attr('src');       // В переменную записывается адрес последнего изображения
-      $('.thumbnails .active').removeClass('active');                  // Удаляется класс .active с предыдущей миниатюры
-      $('.thumbnails a:last').addClass('active');                      // На последнюю миниатюру вешается класс .active
-    }
-    $('.big-image img').attr({ src: prevSrc });                        // Подменяется адрес большого изображения на адрес следующего
-    return false;
-  });
-})
+$(document).ready(function() { // Ждём загрузки страницы
+	$(".image").click(function(){	// Событие клика на маленькое изображение
+	  	var img = $(this);	// Получаем изображение, на которое кликнули
+		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
+		$("main").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
+						 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
+						 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
+						 "</div>"); 
+		$(".popup").fadeIn(800); // Медленно выводим изображение
+		$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+			$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+			setTimeout(function() {	// Выставляем таймер
+			  $(".popup").remove(); // Удаляем разметку всплывающего окна
+			}, 800);
+		});
+	});
+});
