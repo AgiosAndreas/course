29a2dff5100f82c85d058290c26733bbfde45904
Space7Panda <space7panda@gmail.com>
2018-10-16 15:25:29 +0300

    исправил ошибки

--- 05 JavaScript Basics/homework/Space7Panda/2/js/customSort.js
+++ 05 JavaScript Basics/homework/Space7Panda/2/js/customSort.js
@@ -2,10 +2,9 @@
 function startCustomSort() {
 	let value = prompt('Укажите числа через пробел.', '1 3 2');
 
-	if (sort(value) == undefined) {
-		alert('Ошибка: пожалуйста укажите целые числа');
-		return;
+	try {
+		alert(`[${sort(value)}]`);
+	} catch(e) {
+		alert("Ошибка: " + e.Message +"." + "\nПожалуйста, укажите целые числа через пробел.")
 	}
-
-	alert(`[${sort(value)}]`);
 };
