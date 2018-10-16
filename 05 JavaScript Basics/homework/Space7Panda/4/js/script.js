02c2084942be47a69a7243326f5b882eba5771d3
Space7Panda <space7panda@gmail.com>
2018-10-16 16:30:16 +0300

    исправил обработку исключений

--- 05 JavaScript Basics/homework/Space7Panda/4/js/script.js
+++ 05 JavaScript Basics/homework/Space7Panda/4/js/script.js
@@ -1,19 +1,13 @@
 "use strict";
 function factorial(n) {
-	try {
 
-		if (Number.isInteger(n) === false) {
-			throw {"Message": "'n' - is not a number"};
-		}
-		
-		if (n < 0) {
-			throw {"Message": "n < 0"};
-		}
-
-		return (n <= 1) ? 1 : n * factorial(n - 1);
-
-	} catch (e) {
-
-		console.log("Error: " + e.Message);
+	if (Number.isInteger(n) === false) {
+		throw {"Error": "'n' - is not a number"};
 	}
+
+	if (n < 0) {
+		throw {"Error": "n < 0"};
+	}
+
+	return (n <= 1) ? 1 : n * factorial(n - 1);
 };
