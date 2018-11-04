031af2111aeed9d7a37c9b6ef1068535f7ccc9e8
Space7Panda <space7panda@gmail.com>
2018-11-04 13:06:17 +0200

    fix 2/3

--- 06 jQuery/homework/Space7Panda/5/js/api.github.js
+++ 06 jQuery/homework/Space7Panda/5/js/api.github.js
@@ -5,48 +5,59 @@ function ghSearch() {
 
 	$(".loading-container").show();
 
+	if (username == "") {
+
+		$(".loading-container").hide();
+		$(".alert-danger").text("Error: input field is empty").animateCustom()
+	
+		return;
+	}
+
 	let request = $.ajax({url: "https://api.github.com/users/" + username}).promise();
 	
-	setTimeout(function () {	//Why? Для эстетики.
+	setTimeout(function () {
 	
-		request.done(function(data) {
-
-			$("#user-avatar").attr("src", data.avatar_url);
-			$("#user-name").text(data.name);
-			$("#user-nickname").text(data.login);
-			$("#user-since").text("Since: " + data.created_at.slice(0, 4))
-			$("#user-followers").text(data.followers);
-			$("#user-repos").text(data.public_repos);
-			$("#user-following").text(data.following);
-			$("#user-link").attr("href", data.html_url)
-			$(".loading-container").hide();
-			$(".output-container").slideDown(1000);
-			$(".alert-success").text("User '"+ username +"' successfully found!").animateCustom()
+		request.done(function (data) {
+			
+			searchSuccess(data);
 
 		}).fail(function(data) {
 
-			if (username == "") {
+			searchFail(data);
 
-				$(".loading-container").hide();
-				$(".alert-danger").text("Error: input field is empty").animateCustom()
-			
-				return;
-			}
-
-			if (data.responseJSON.message === "Not Found") {
-
-				$(".loading-container").hide();
-				$(".alert-danger").text("Error: username is " + data.responseJSON.message ).animateCustom()
-
-			} else {
-
-				$(".loading-container").hide();
-				$(".alert-danger").text("Error! Check console.").animateCustom()
-
-			}
 		});
 	}, 2000);
 
+}
+
+function searchSuccess(data) {
+	
+	$("#user-avatar").attr("src", data.avatar_url);
+	$("#user-name").text(data.name);
+	$("#user-nickname").text(data.login);
+	$("#user-since").text("Since: " + data.created_at.slice(0, 4))
+	$("#user-followers").text(data.followers);
+	$("#user-repos").text(data.public_repos);
+	$("#user-following").text(data.following);
+	$("#user-link").attr("href", data.html_url)
+	$(".loading-container").hide();
+	$(".output-container").slideDown(1000);
+	$(".alert-success").text("User '"+ data.login +"' successfully found!").animateCustom();
+}
+
+function searchFail(data) {
+	
+	if (data.responseJSON.message === "Not Found") {
+
+		$(".loading-container").hide();
+		$(".alert-danger").text("Error: username is " + data.responseJSON.message ).animateCustom()
+
+	} else {
+
+		$(".loading-container").hide();
+		$(".alert-danger").text("Error! Check console.").animateCustom()
+
+	}
 }
 
 (function( $ ){
