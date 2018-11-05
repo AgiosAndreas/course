2254bdfbd514df26bbbdf7eaf9369e18ca51fee6
Space7Panda <space7panda@gmail.com>
2018-11-05 13:10:43 +0200

    first commit

---
+++ 07 Node/homework/Space7Panda/2/decoder.js
@@ -0,0 +1,10 @@
+"use strict";
+const ABC          = require('./alphabet.json')
+const SPLIT_LETTER = "   ";
+const SPLIT_WORD   = "       ";
+
+class MorseDecoder {
+
+}
+
+module.exports = MorseDecoder;
