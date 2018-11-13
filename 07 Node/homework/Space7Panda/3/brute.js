960f739b5bb82a66a152a014063d71334ccff3fb
Space7Panda <space7panda@gmail.com>
2018-11-13 13:19:39 +0200

    some ugly code

--- 07 Node/homework/Space7Panda/3/brute.js
+++ 07 Node/homework/Space7Panda/3/brute.js
@@ -1,49 +1,18 @@
 "use strict";
 const G = require('generatorics');
 const sha256 = require('js-sha256');
-const fs = require('fs');
 
-let path = 'hashes';
+function bruteSha256(code, letters) {
 
-fs.readdir(path, 'utf-8', function (err, dirFiles) {
-  if (err) {
-    return console.log(err)
-  }
+	for (var perm of G.permutation(letters)) {
 
-  for (var i = 0; i < dirFiles.length; i++) {
-    fileProcessing(path, dirFiles[i]);
-  }
-})
+		let word = perm.join("");
+		let encodedWord = sha256(word);
 
-function  fileProcessing(path, fileName) {
-  fs.readFile(path + "//" + fileName, "utf8", function(err, content){
-    if (err) {
-      return console.log(err)
-    }
-
-    bruteSha256(content);
-  })
-}
-
-function bruteSha256(content) {
-
-  content = content.split(" ");
-  let code = content[0];
-  let letters = content[1].split("");
-
-  for (var perm of G.permutation(letters)) {
-
-    let word = perm.join("");
-    let encodedWord = sha256(word);
-
-    if (encodedWord == code) {
-      console.log("success");
-      console.log(word);
-      console.log(encodedWord);
-
-      return word;
-    }
-  }
+		if (encodedWord == code) {
+			return word;
+		}
+	}
 }
 
 module.exports = bruteSha256;
