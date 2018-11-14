90af0cc546f5e158c3b659edf3ef0b6d737ad74f
Space7Panda <space7panda@gmail.com>
2018-11-14 15:06:44 +0200

    add class

--- 07 Node/homework/Space7Panda/3/brute.js
+++ 07 Node/homework/Space7Panda/3/brute.js
@@ -1,18 +1,58 @@
 "use strict";
 const G = require('generatorics');
 const sha256 = require('js-sha256');
+const fs = require('fs');
 
-function bruteSha256(code, letters) {
+class Bruter {
 
-	for (var perm of G.permutation(letters)) {
+	brute(path) {
 
-		let word = perm.join("");
-		let encodedWord = sha256(word);
+		fs.readdir(path, 'utf-8', function (err, dirFiles) {
+			if (err) {
+				return console.log(err)
+			}
 
-		if (encodedWord == code) {
-			return word;
+			
+			for (var i = 0; i < dirFiles.length; i++) {
+				fileProcessing(path, dirFiles[i]);
+			}
+		})
+
+		function fileProcessing(path, fileName) {
+			fs.readFile(path + "//" + fileName, "utf8", function(err, content){
+				if (err) {
+					return console.log(err)
+				}
+		
+				content = content.split(" ");
+				let code = content[0];
+				let letters = content[1].split("");
+		
+				let word = bruteSha256(code, letters);
+		
+				if (word == undefined) {
+					word = null;
+				}
+				
+				let result = fileName + " " + word + " " + code;
+				
+				console.log(result);
+			})
+		}
+		
+		function bruteSha256(code, letters) {
+		
+			for (var perm of G.permutation(letters)) {
+		
+				let word = perm.join("");
+				let encodedWord = sha256(word);
+		
+				if (encodedWord == code) {
+					return word;
+				}
+			}
 		}
 	}
 }
 
-module.exports = bruteSha256;
+module.exports = Bruter;
