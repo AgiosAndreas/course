c698361877cbbdcebcff2f087a00ad61de8eb3b0
MirshaAndrey <mirsha.andrey@gmail.com>
2018-11-22 01:02:01 +0200

    ex-06-05 fix display spinner during query execution

--- 06 jQuery/homework/MirshaAndrey/05/js/script.js
+++ 06 jQuery/homework/MirshaAndrey/05/js/script.js
@@ -1,27 +1,46 @@
 $(function() {
   $("#search-user").submit(function(event) {
     event.preventDefault();
-    gitsearch();
+    gitSearch();
   });
-  
-  function gitsearch() {
+
+  function gitSearch() {
     var userName = $("#get-userName").val().trim();
-    $("#waitdata").show();
+    if (userName == "") {
+      var errorText = ("Please, spesify the userName");
+      showError(errorText);
+      return;
+    }
     var url = "https://api.github.com/users/" + userName;
-    $.get(url)
+    $("#gitLoad").show(); $.getJSON(url)
       .done(function(data) {
-        $("#avatar").attr("src", data.avatar_url);
-        $("#usernamepr").text(data.name);
-        $("#git-year").text("since " + data.created_at.slice(0, 4));
+        $("#userNamepr").text(data.name);
         $("#followers").text(data.followers);
         $("#repositories").text(data.public_repos);
         $("#following").text(data.following);
-        $("#profile").show();
-        $("#waitdata").hide("slow");
-        $("#user-profile")
+        $("#gitProfile").show();
+        $("#gitYear").text("since " + data.created_at.slice(0, 4));
+        $("#gitLoad").hide("slow");
+        $("#avatar").attr("src", data.avatar_url);
+        $("#userProfile")
           .empty()
           .append(userName);
         $("#git-userLink").attr("href", "https://github.com/" + userName);
-      })  
-  };
+      })
+      .fail(function(data, textStatus, error) {
+        var errorText = ("User <strong>" + userName + "</strong> " + error.toLowerCase());
+        $("#gitLoad").hide("slow");
+        showError(errorText);
+      });
+  }
+
+  function showError(errorText) {
+    $(".alert-danger")
+      .empty()
+      .append(errorText)
+      .show();
+    setTimeout(function() {
+      $(".alert-danger").hide("slow");
+    }, 2000);
+  }
 });
