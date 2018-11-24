12fbcacb915cb07fb60cd8ec1c507cb7cb54f2b8
Space7Panda <space7panda@gmail.com>
2018-11-24 18:30:28 +0200

    optimize code

---
+++ 07 Node/homework/Space7Panda/2/test/decoder_test.js
@@ -0,0 +1,82 @@
+"use strict";
+const assert = require('assert');
+const Decoder = require("../decoder.js");
+
+describe("Decoder", function() {
+
+	let abc = require('../abc/alphabet.json');
+	let split_letter = "   ";
+	let split_word = "       ";
+
+	let abc_RU = require('../abc/alphabetRU.json');
+	let split_letter_RU = "  ";
+	let split_word_RU = "    ";
+
+	let decoder = [
+		new Decoder(abc, split_letter, split_word), 
+		new Decoder(abc_RU, split_letter_RU, split_word_RU)
+	];
+
+	let morseCodes = [{
+		code:'. . . .   .   . - . .   . - - .   . . - - . .',
+		word:'HELP?'
+	},
+	{
+		code:'. . .   - - -   . . .',
+		word:'SOS'
+	},
+	{
+		code:'. . . .   .   . - . .   . - . .   - - -       . - -   - - -   . - .   . - . .   - . .   - - . . - -',
+		word:'HELLO WORLD!'
+	}];
+
+	let morseCodesRu = [{
+		code:'. - - .  . - .  . .  . - -  .  -    - -  . .  . - .  - - . . - -',
+		word:'ПРИВЕТ МИР!'
+	},
+	{
+		code:'. - - - - .  . - . -  . - . - . -    . - -  . -  . . .  . - . -  . . . . . .  . - - - - .',
+		word:"'Я, ВАСЯ.'"
+	}]
+
+	describe("contract test", function() {
+
+		for (let i=0; i < decoder.length; i++) {
+
+			it(`decoder[${[i]}].decode('') return empty string`, function() {
+				assert.equal(decoder[i].decode(''), '');
+			});
+
+			it(`decoder[${[i]}].decode('null') return empty string`, function() {
+				assert.equal(decoder[i].decode(null), '');
+			});
+
+			it(`decoder[${[i]}] if cant find word return empty string`, function() {
+				assert.equal(decoder[i].decode('. - - - -   - - - - - -  . - ---'), '');
+			});
+		}
+	})
+
+	describe("morse decode", function() {
+
+		for (let i = 0; i < morseCodes.length; i++) {
+
+			it (`decode ${morseCodes[i].word}`, function() {
+				assert.equal(decoder[0].decode(morseCodes[i].code), morseCodes[i].word);
+			})
+
+		}
+		
+	})
+
+	describe("morseRU decode", function() {
+
+		for (let i = 0; i < morseCodesRu.length; i++) {
+
+			it (`decode ${morseCodesRu[i].word}`, function() {
+				assert.equal(decoder[1].decode(morseCodesRu[i].code), morseCodesRu[i].word);
+			})
+
+		}
+	})
+});
