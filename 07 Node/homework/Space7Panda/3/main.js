18443fe6565feaeea0399f30f8eaaee75ea5ff9f
Space7Panda <space7panda@gmail.com>
2018-12-02 11:05:26 +0200

    add new class method

--- 07 Node/homework/Space7Panda/3/main.js
+++ 07 Node/homework/Space7Panda/3/main.js
@@ -3,35 +3,8 @@ if (process.argv.length > 3) {
 	throw new Error('to many arguments');
 }
 
-const bruteSha256 = require('./brute.js')
-const fs = require('fs-extra')
+const BruteSha256 = require('./brute.js')
 const path = process.argv[2];
-const sha256 = new bruteSha256;
+const sha256 = new BruteSha256;
 
-fs.readdir(path, 'utf8')
-.then(data => {
-
-	let dirData = {
-		files: data,
-		path: path
-	}
-
-	console.log(`Successfully found ${dirData.files.length} files in catalog ${dirData.path}:`);
-	console.log(dirData.files);
-
-	return dirData;
-})
-.then(data => {
-
-	console.log("\n bruting...\n");
-	
-	for (let i = 0; i < data.files.length; i++) {
-
-		fs.readFile(data.path + "//" + data.files[i], "utf8").then(content => {
-			console.log(sha256.bruteFile(data.files[i], content));
-		})
-	}
-})
-.catch(err => {
-	console.error(err)
-})
+sha256.bruteDir(path);
