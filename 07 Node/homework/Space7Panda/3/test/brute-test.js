af9682aab968c2ac2f24d5b510a88f21a03986c2
Space7Panda <space7panda@gmail.com>
2018-12-03 11:35:56 +0200

    rewriting test

--- 07 Node/homework/Space7Panda/3/test/brute-test.js
+++ 07 Node/homework/Space7Panda/3/test/brute-test.js
@@ -1,73 +1,60 @@
 const assert = require('assert');
-const bruteSha256 = require('../brute.js')
-const sha256 = new bruteSha256;
+const BruteSha256 = require('../brute.js')
+const sha256 = new BruteSha256;
 
-describe("sha256", function() { 
+describe("sha256", () => { 
 
-	let files = [
-	{
-		name:'file_1',
-		code:'2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-		letters:'elloh',
-		word:'hello'
-	},
-	{
-		name:'file_2',
-		code:'486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7',
-		letters:'dlwor',
-		word:'world'
-	},
-	{
-		name:'file_3',
-		code:'8997fb8c0b5c3a08ec0a7bcbac77dae2074c3cf0eaf0845bcc9e275040c64994',
-		letters:'oyfzz',
-		word:'fozzy'
-	},
-	{
-		name:'file_4',
-		code:'f9194e73f9e9459e3450ea10a179cdf77aafa695beecd3b9344a98d111622243',
-		letters:'eroz',
-		word:'zero'
-	},
-	{
-		name:'file_5',
-		code:'cd6357efdd966de8c0cb2f876cc89ec74ce35f0968e11743987084bd42fb8944',
-		letters:'god',
-		word:'dog'
-	}];
+	describe("brute contract test", () => {
 
-	describe("brute contract", function() {
-
-		it("if 'code' or 'letter' empty return 'null'", function() {
+		it("if 'code' or 'letter' empty return 'null'", () => {
 			assert.equal(sha256.brute('', ''), null);
 		});
 
-		it("if cant find key return 'null'", function() {
-			assert.equal(sha256.brute('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', 'asdds'), null);
+		it("if cant find key return 'null'", () => {
+			assert.equal(sha256.brute('2cf244', 'asdds'), null);
 		});
 
 	 })
 
-	describe("sha256.brute(code, letters)", function() {
+	describe("sha256.brute(code, letters)", () => {
 
-		for (let i = 0; i < files.length; i++) {
+		let codes = [
+		{
+			code:'2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+			letters:'elloh',
+			word:'hello'
+		},
+		{
+			code:'486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7',
+			letters:'dlwor',
+			word:'world'
+		},
+		{
+			code:'8997fb8c0b5c3a08ec0a7bcbac77dae2074c3cf0eaf0845bcc9e275040c64994',
+			letters:'oyfzz',
+			word:'fozzy'
+		},
+		{
+			code:'f9194e73f9e9459e3450ea10a179cdf77aafa695beecd3b9344a98d111622243',
+			letters:'eroz',
+			word:'zero'
+		},
+		{
+			code:'cd6357efdd966de8c0cb2f876cc89ec74ce35f0968e11743987084bd42fb8944',
+			letters:'god',
+			word:'dog'
+		}];
 
-			it(`find key '${files[i].word}'`, function() {
-				assert.equal(sha256.brute(files[i].code, files[i].letters), files[i].word);
+		for (let i = 0; i < codes.length; i++) {
+
+			let code = codes[i].code;
+			let letters = codes[i].letters;
+			let result = codes[i].word;
+
+			it(`find key '${result}'`, () => {
+				assert.equal(sha256.brute(code, letters), result);
 			});
 
 		}
-	})
-
-	describe("sha256.bruteFile(fileName, content)", function() {
-
-		for (let i = 0; i < files.length; i++) {
-
-			it('returns ' + files[i].name + " " + files[i].word + " " + files[i].code, function() {
-				assert.equal(sha256.bruteFile(files[i].name, files[i].code + ' ' + files[i].letters), files[i].name + " " + files[i].word + " " + files[i].code);
-			});
-	
-		}
-
 	})
 });
