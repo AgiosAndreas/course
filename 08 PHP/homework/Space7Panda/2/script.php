fc3237dd158e12af979d30b0122cddd14b294509
Space7Panda <space7panda@gmail.com>
2018-12-18 11:21:24 +0200

    add contract

--- 08 PHP/homework/Space7Panda/2/script.php
+++ 08 PHP/homework/Space7Panda/2/script.php
@@ -1,12 +1,33 @@
 <?php
 
 function sum_pairs($values, $expectedSum) {
-	
+
+	if (is_array($values) == false) {
+		throw new Exception("$values is not array");
+	}
+
+	if (is_int($expectedSum) == false) {
+		throw new Exception("$expectedSum is not integer");
+	}
+
+	if (count($values) == 1) {
+		throw new Exception("not enough arguments in array");
+	}
+
 	for ($i = 0; $i + 1 < count($values); $i++) {
+
+		if (is_int($values[$i]) == false) {
+			throw new Exception("$values[$i] is not integer");
+		}
 
 		$firstValue = $values[$i];
 
 		for ($j = $i + 1; $j < count($values); $j++) {
+
+			if (is_int($values[$j]) == false) {
+				throw new Exception("$values[$j] is not integer");
+			}
+
 			$secondValue = $values[$j];
 			$sum = $firstValue + $secondValue;
 			
