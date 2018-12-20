86b039539225cba9242ff38c070d5dd301a02a60
Space7Panda <space7panda@gmail.com>
2018-12-20 21:33:38 +0200

    not good enough

--- 08 PHP/homework/Space7Panda/2/script.php
+++ 08 PHP/homework/Space7Panda/2/script.php
@@ -10,37 +10,27 @@ function sum_pairs($values, $expectedSum) {
 		throw new Exception("$expectedSum is not integer");
 	}
 
-	if (count($values) == 1) {
+	$arrCount = count($values);
+
+	if ($arrCount < 2) {
 		throw new Exception("not enough arguments in array");
 	}
+	
+	for ($i = 0; $i < $arrCount; $i++) {
 
-	for ($i = 0; $i + 1 < count($values); $i++) {
+		$find[$i] = $expectedSum - $values[$i];
 
-		if (is_int($values[$i]) == false) {
-			throw new Exception("$values[$i] is not integer");
+		if (in_array($values[$i], $find) && $values[$i] != $find[$i]) {
+			echo "[$find[$i], $values[$i]]\n";
+			return;
 		}
 
-		$firstValue = $values[$i];
-
-		for ($j = $i + 1; $j < count($values); $j++) {
-
-			if (is_int($values[$j]) == false) {
-				throw new Exception("$values[$j] is not integer");
-			}
-
-			$secondValue = $values[$j];
-			$sum = $firstValue + $secondValue;
-			
-			if ($sum == $expectedSum) {
-				return "[$firstValue, $secondValue]";
-			}
-		}
 	}
 
 	return NULL;
 }
 
-echo sum_pairs([11, 3, 7, 5], 10) . "\n";
-echo sum_pairs([4, 3, 2, 3, 4], 6) . "\n";
-echo sum_pairs([10, 5, 2, 3, 7, 5], 10) . "\n";
-echo sum_pairs([0, 0, -2, 3], 2) . "\n";
+echo sum_pairs([11, 3, 7, 5], 10) 
+	. sum_pairs([4, 3, 2, 3, 4], 6) 
+	. sum_pairs([10, 5, 2, 3, 7, 5], 10)
+	. sum_pairs([0, 0, -2, 3], 2);
