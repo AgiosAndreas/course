78a18a7bf64031b08337c31d3ae48acfdd22d3ff
MirshaAndrey <mirsha.andrey@gmail.com>
2018-12-27 05:45:27 +0200

    ex-07-02 simplified functions str74-82

--- 07 Node/homework/MirshaAndrey/02/ex-07-02.js
+++ 07 Node/homework/MirshaAndrey/02/ex-07-02.js
@@ -4,7 +4,7 @@ let MorseDecod = require("./morseDecod.js");
 
 let morse = new MorseDecod();
 
-console.log(morse.decode(". .       . - . .   - - -   . . . -   .       - .   - - -   - . .   .   . - - -   . . .   . - . - . -   -. - . - -   -. - . - -   -. - . - -"));
+console.log(morse.decode(". .       . - . .   - - -   . . . -   .       - .   - - -   - . .   .   . - - -   . . .   . - . - . -   -. - . - -   -. - . - -   -. - . - -")); //i love nodejs.!!!
 console.log(morse.decode(". . . .   .   . - . .   . - - .")); // 'HELP'
 console.log(morse.decode(". . .   - - -   . . .")); // 'SOS'
 console.log(morse.decode(". - - - -   - - - - - -  . - ---")); // ''
--- 07 Node/homework/MirshaAndrey/02/morseDecod.js
+++ 07 Node/homework/MirshaAndrey/02/morseDecod.js
@@ -1,81 +1,87 @@
-"use strict";
+"use strict"
 class MorseDecod {
-	decode(morseCode) {
-		const lettersSpace = "   ";
-		const wordSpace = "       ";
-		const alphabet = {
-			// letters
-			". -": "a",
-			"- . . .": "b",
-			"- . - .": "c",
-			"- . .": "d",
-			".": "e",
-			". . - .": "f",
-			"- - .": "g",
-			". . . .": "h",
-			". .": "i",
-			". - - -": "j",
-			"- . -": "k",
-			". - . .": "l",
-			"- -": "m",
-			"- .": "n",
-			"- - -": "o",
-			". - - .": "p",
-			"- - . -": "q",
-			". - .": "r",
-			". . .": "s",
-			"-": "t",
-			". . -": "u",
-			". . . -": "v",
-			". - -": "w",
-			"- . . -": "x",
-			"- . - -": "y",
-			"- - . .": "z",
-			". - - - -": "1",
-			". . - - -": "2",
-			". . . - -": "3",
-			". . . . -": "4",
-			". . . . .": "5",
-			"- . . . .": "6",
-			"- - . . .": "7",
-			"- - - . .": "8",
-			"- - - - .": "9",
-			"- - - - -": "0",
-			". - . - . -": ".",
-			"- - . . - -": ",",
-			".. - - . .": "?",
-			". - - - - .": "'",
-			"-. - . - -": "!",
-			"- . . - .": "/",
-			"- . - - .": "(",
-			"- . - - . -": ")",
-			". - . . .": "&",
-			"- - - . . .": ":",
-			"- . - . - .": ";",
-			"- . . . -": "=",
-			". - . - .": "+",
-			"- . . . . ": "- ",
-			". . - - . -": "_",
-			". - . . - .": '"',
-			". . . - . . -": "$",
-			". - - . - .": "@"
-		};
-		if (typeof morseCode !== "string") return " ";
-		morseCode = morseCode.trim();
-		if (morseCode.length == 0) return "";
-		morseCode = morseCode
-			.split(wordSpace)
-			.map(function(word) {
-				let letters = word.split(lettersSpace);
-				return (
-					(word = letters
-						.map(word => (word in alphabet ? alphabet[word] : ""))
-						.join("")),
-					letters.length == word.length ? word : ""
-				);
-			})
-			.join(" ");
-		return morseCode;
-	}
+    decode(morseCode) {
+        const lettersSpace = "   "
+        const wordSpace = "       "
+        const alphabet = {
+            // letters
+            ". -": "a",
+            "- . . .": "b",
+            "- . - .": "c",
+            "- . .": "d",
+            ".": "e",
+            ". . - .": "f",
+            "- - .": "g",
+            ". . . .": "h",
+            ". .": "i",
+            ". - - -": "j",
+            "- . -": "k",
+            ". - . .": "l",
+            "- -": "m",
+            "- .": "n",
+            "- - -": "o",
+            ". - - .": "p",
+            "- - . -": "q",
+            ". - .": "r",
+            ". . .": "s",
+            "-": "t",
+            ". . -": "u",
+            ". . . -": "v",
+            ". - -": "w",
+            "- . . -": "x",
+            "- . - -": "y",
+            "- - . .": "z",
+            ". - - - -": "1",
+            ". . - - -": "2",
+            ". . . - -": "3",
+            ". . . . -": "4",
+            ". . . . .": "5",
+            "- . . . .": "6",
+            "- - . . .": "7",
+            "- - - . .": "8",
+            "- - - - .": "9",
+            "- - - - -": "0",
+            ". - . - . -": ".",
+            "- - . . - -": ",",
+            ".. - - . .": "?",
+            ". - - - - .": "'",
+            "-. - . - -": "!",
+            "- . . - .": "/",
+            "- . - - .": "(",
+            "- . - - . -": ")",
+            ". - . . .": "&",
+            "- - - . . .": ":",
+            "- . - . - .": ";",
+            "- . . . -": "=",
+            ". - . - .": "+",
+            "- . . . . ": "- ",
+            ". . - - . -": "_",
+            ". - . . - .": '"',
+            ". . . - . . -": "$",
+            ". - - . - .": "@",
+        }
+
+        if (typeof morseCode !== "string") return " "
+        morseCode = morseCode.trim()
+
+        if (morseCode.length == 0) return ""
+
+        morseCode = morseCode
+            .split(wordSpace)
+            .map(function(word) {
+                let letters = word.split(lettersSpace)
+
+                word = letters
+                    .map(word => (word in alphabet ? alphabet[word] : ""))
+                    .join("")
+
+                let lettersLength = letters.length == word.length ? word : ""
+
+                return word, lettersLength
+            })
+            .join(" ")
+
+        return morseCode
+    }
 }
-module.exports = MorseDecod;
+module.exports = MorseDecod
