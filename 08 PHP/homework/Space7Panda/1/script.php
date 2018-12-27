7c5e40b5d10bcdaa42ac8ff894617b4706f68585
Space7Panda <space7panda@gmail.com>
2018-12-27 18:33:05 +0200

    add validate func

--- 08 PHP/homework/Space7Panda/1/script.php
+++ 08 PHP/homework/Space7Panda/1/script.php
@@ -1,18 +1,18 @@
 <?php
 
+function validate($variable) {
+
+	if (!is_int($variable) || $variable < 0 || $variable > 255) {
+		throw new Exception("Incorrect value for variable ($variable) Value must be integer that >= 0 and < 256");
+	}
+
+}
+
 function rgb($r, $g, $b) {
 
-	if (!is_int($r) || $r < 0 || $r > 255) {
-		throw new Exception("Incorrect value for variable 'r':($r) Value must be integer that >= 0 and < 256");
-	}
-
-	if (!is_int($g) || $g < 0 || $g > 255) {
-		throw new Exception("Incorrect value for variable 'g':($g) Value must be integer that >= 0 and < 256");
-	}
-
-	if (!is_int($b) || $b < 0 || $b > 255) {
-		throw new Exception("Incorrect value for variable 'b':($b) Value must be integer that >= 0 and < 256");
-	}
+	validate($r);
+	validate($g);
+	validate($b);
 
 	$rgbMultiplied = $r << 16 | $g << 8 | $b; 
 	
