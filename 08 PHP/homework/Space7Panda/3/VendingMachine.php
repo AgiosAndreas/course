32123a39a1bc4103980bed0d8ff162a939cc3720
Space7Panda <space7panda@gmail.com>
2018-12-30 14:22:59 +0200

    fix

--- 08 PHP/homework/Space7Panda/3/VendingMachine.php
+++ 08 PHP/homework/Space7Panda/3/VendingMachine.php
@@ -7,6 +7,7 @@ interface VendingMachineInterface
 
 interface DefaultProductInterface
 {
+    public function getCode($code);
     public function getName($code);
     public function getQuantity($code);
     public function getPrice($code);
@@ -30,6 +31,15 @@ class DefaultProduct implements DefaultProductInterface
         }
 
         return $sortedArr;
+    }
+
+    public function getCode($code)
+    {
+        if ($this->items[$code]) {
+            return true;
+        }
+
+        return false;
     }
 
     public function getName($code) 
@@ -75,6 +85,11 @@ class VendingMachine implements VendingMachineInterface
 
     public function vend($code, $cash) 
     {
+        if ($this->product->getCode($code) == false) {
+            echo "Такого товара нет в автомате!\n";
+
+            return;
+        }
 
         $name = $this->product->getName($code);
         $quantity = $this->product->getQuantity($code);
@@ -132,50 +147,3 @@ class VendingMachine implements VendingMachineInterface
         }
     }
 }
-
-$items = [
-    [ "name" => "Шоколад белый", "code" => "A01", "quantity" => 10, "price" => 0.60 ],
-    [ "name" => "Шоколад молочный", "code" => "A02", "quantity" => 5, "price" => 0.60 ],
-    [ "name" => "Пиво светлое", "code" => "A03", "quantity" => 1, "price" => 0.65 ],
-    [ "name" => "Вода без газа", "code" => "A04", "quantity" => 1, "price" => 0.25 ]
-];
-
-$normalProduct = new DefaultProduct($items);
-$dispenser = new VendingMachine($normalProduct);
-
-$dispenser->vend('A01', 6);
-$dispenser->vend('A02', 6);
-$dispenser->vend('A03', 6);
-$dispenser->showCash();
-$dispenser->showItems();
-
-require_once("products/Alcohol.php");
-
-$alcoholItems = [
-    [ "name" => "Водка", "code" => "A01", "quantity" => 3, "price" =>  1 ],
-    [ "name" => "Вино", "code" => "A02", "quantity" => 7, "price" => 2.60 ],
-    [ "name" => "Пиво светлое", "code" => "A03", "quantity" => 15, "price" => 1.65 ],
-    [ "name" => "Самогон", "code" => "A04", "quantity" => 1, "price" => 2.25 ]
-];
-
-$alcohol = new Alcohol($alcoholItems);
-$dispenserWithAlcohol = new VendingMachine($alcohol);
-
-$dispenserWithAlcohol->vend('A01', 5);
-$dispenserWithAlcohol->vend('A02', 10);
-$dispenserWithAlcohol->vend('A03', 15);
-
-require_once("products/ProductsWithExpDate.php");
-
-$expDateItems = [
-    [ "name" => "Колбаса", "code" => "A01", "quantity" => 1, "price" => 2.05, "expiration date" => "20.12.2018"],
-    [ "name" => "Консервы", "code" => "A02", "quantity" => 10, "price" => 1.05, "expiration date" => "20.12.2025"],
-    [ "name" => "Борщ", "code" => "A03", "quantity" => 1, "price" => 0.05, "expiration date" => "01.01.2007"],
-];
-
-$productsWithExpDate = new WithExpDate($expDateItems);
-$dispenserWithExpDate = new VendingMachine($productsWithExpDate);
-
-$dispenserWithExpDate->vend('A01', 5);
-$dispenserWithExpDate->vend('A02', 10);
-$dispenserWithExpDate->vend('A03', 15);
