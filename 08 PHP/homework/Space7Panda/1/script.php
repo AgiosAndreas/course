4171f41c86a27236f0a21f998f022818e0b9001a
Space7Panda <space7panda@gmail.com>
2019-01-02 16:22:48 +0200

    validate PSR2

--- 08 PHP/homework/Space7Panda/1/script.php
+++ 08 PHP/homework/Space7Panda/1/script.php
@@ -1,24 +1,24 @@
 <?php
 
-function validate($variable) {
-
-	if (!is_int($variable) || $variable < 0 || $variable > 255) {
-		throw new Exception("Incorrect value for variable ($variable) Value must be integer that >= 0 and < 256");
-	}
-
+function validate($variable)
+{
+    if (!is_int($variable) || $variable < 0 || $variable > 255) {
+        throw new Exception("Incorrect value for variable ($variable) Value must be integer that >= 0 and < 256");
+    }
 }
 
-function rgb($r, $g, $b) {
+function rgb($r, $g, $b)
+{
 
-	validate($r);
-	validate($g);
-	validate($b);
+    validate($r);
+    validate($g);
+    validate($b);
 
-	$rgbMultiplied = $r << 16 | $g << 8 | $b; 
-	
-	$result = dechex($rgbMultiplied);
-	
-	return strtoupper(("#".substr("000000".$result, -6)));
+    $rgbMultiplied = $r << 16 | $g << 8 | $b;
+
+    $result = dechex($rgbMultiplied);
+    
+    return strtoupper(("#".substr("000000".$result, -6)));
 }
 
 echo rgb(255, 255, 255) . "\n";
