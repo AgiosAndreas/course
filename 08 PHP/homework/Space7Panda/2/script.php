690d0f0d22cc34ac25c3b63b247172ed717d32d2
Space7Panda <space7panda@gmail.com>
2019-01-02 17:05:02 +0200

    validate PSR2

--- 08 PHP/homework/Space7Panda/2/script.php
+++ 08 PHP/homework/Space7Panda/2/script.php
@@ -1,58 +1,54 @@
-	<?php
+    <?php
 
-	function sum_pairs($values, $expectedSum) {
+    function sum_pairs($values, $expectedSum)
+    {
+        if (is_array($values) == false) {
+            throw new Exception("$values is not array");
+        }
 
-		if (is_array($values) == false) {
-			throw new Exception("$values is not array");
-		}
+        if (is_int($expectedSum) == false) {
+            throw new Exception("$expectedSum is not integer");
+        }
 
-		if (is_int($expectedSum) == false) {
-			throw new Exception("$expectedSum is not integer");
-		}
+        $arrCount = count($values);
 
-		$arrCount = count($values);
+        if ($arrCount < 2) {
+            throw new Exception("not enough arguments in array");
+        }
+        
+        $find = array();
 
-		if ($arrCount < 2) {
-			throw new Exception("not enough arguments in array");
-		}
-		
-		$find = array();
+        for ($i = 0; $i < $arrCount; $i++) {
+            $currentValue = $values[$i];
 
-		for ($i = 0; $i < $arrCount; $i++) {
+            $expectedValue = $expectedSum - $currentValue;
 
-			$currentValue = $values[$i];
+            if ($find[$currentValue] == true) {
+                $firstValue = $expectedSum - $currentValue;
 
-			$expectedValue = $expectedSum - $currentValue;
+                return [$firstValue, $currentValue];
+            }
 
-			if ($find[$currentValue] == true) {
+            $find[$expectedValue] = true;
+        }
 
-				$firstValue = $expectedSum - $currentValue;
+        return null;
+    }
 
-				return [$firstValue, $currentValue];
-			}
+    function test_sum_pairs($function, $expectedResult)
+    {
+        if (!is_array($expectedResult) && !is_null($expectedResult)) {
+            throw new Exception("$expectedResult is not array or null");
+        }
 
-			$find[$expectedValue] = true;
+        if ($function == $expectedResult) {
+            echo "Successfull test";
+        } else {
+            echo "Test is failed";
+        }
+    }
 
-		}
-
-		return NULL;
-	}
-
-	function test_sum_pairs($function, $expectedResult) {
-
-		if (!is_array($expectedResult) && !is_null($expectedResult)) {
-			throw new Exception("$expectedResult is not array or null");
-		}
-
-		if ($function == $expectedResult) {
-			echo "Successfull test";
-		} else {
-			echo "Test is failed";
-		}
-
-	}
-
-	test_sum_pairs(sum_pairs([11, 3, 7, 5], 10), [3, 7]);
-	test_sum_pairs(sum_pairs([4, 3, 2, 3, 4], 6), [4, 2]);
-	test_sum_pairs(sum_pairs([10, 5, 2, 3, 7, 5], 10), [3, 7]);
-	test_sum_pairs(sum_pairs([0, 0, -2, 3], 2), null);
+    test_sum_pairs(sum_pairs([11, 3, 7, 5], 10), [3, 7]);
+    test_sum_pairs(sum_pairs([4, 3, 2, 3, 4], 6), [4, 2]);
+    test_sum_pairs(sum_pairs([10, 5, 2, 3, 7, 5], 10), [3, 7]);
+    test_sum_pairs(sum_pairs([0, 0, -2, 3], 2), null);
