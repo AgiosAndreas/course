ff2d5e08822a31df8fc451cf05862390fe391003
Gregory Myagkov <space7panda@Gregorys-MacBook-Air.local>
2019-01-03 19:42:06 +0200

    foramting fix

--- 08 PHP/homework/Space7Panda/3/script.php
+++ 08 PHP/homework/Space7Panda/3/script.php
@@ -4,10 +4,10 @@ require_once "VendingMachine.php";
 require_once "products/ProductBase.php";
 
 $items = [
-    [ "name" => "Шоколад белый", "code" => "A01", "quantity" => 10, "price" => 0.60 ],
-    [ "name" => "Шоколад молочный", "code" => "A02", "quantity" => 5, "price" => 0.60 ],
-    [ "name" => "Пиво светлое", "code" => "A03", "quantity" => 1, "price" => 0.65 ],
-    [ "name" => "Вода без газа", "code" => "A04", "quantity" => 1, "price" => 0.25 ]
+    ["name" => "Шоколад белый", "code" => "A01", "quantity" => 10, "price" => 0.60],
+    ["name" => "Шоколад молочный", "code" => "A02", "quantity" => 5, "price" => 0.60],
+    ["name" => "Пиво светлое", "code" => "A03", "quantity" => 1, "price" => 0.65],
+    ["name" => "Вода без газа", "code" => "A04", "quantity" => 1, "price" => 0.25]
 ];
 
 foreach ($items as $key => $item) {
@@ -25,10 +25,10 @@ $dispenser->showItems();
 require_once "products/Alcohol.php";
 
 $alcoholItems = [
-    [ "name" => "Водка", "code" => "A01", "quantity" => 3, "price" =>  1 ],
-    [ "name" => "Вино", "code" => "A02", "quantity" => 7, "price" => 2.60 ],
-    [ "name" => "Пиво светлое", "code" => "A03", "quantity" => 15, "price" => 1.65 ],
-    [ "name" => "Самогон", "code" => "A04", "quantity" => 1, "price" => 2.25 ]
+    ["name" => "Водка", "code" => "A01", "quantity" => 3, "price" => 1],
+    ["name" => "Вино", "code" => "A02", "quantity" => 7, "price" => 2.60],
+    ["name" => "Пиво светлое", "code" => "A03", "quantity" => 15, "price" => 1.65],
+    ["name" => "Самогон", "code" => "A04", "quantity" => 1, "price" => 2.25]
 ];
 
 foreach ($alcoholItems as $key => $alcItem) {
@@ -44,9 +44,9 @@ $dispenserWithAlcohol->vend('A05', 15);
 require_once "products/ProductsWithExpDate.php";
 
 $expDateItems = [
-    [ "name" => "Колбаса", "code" => "A01", "quantity" => 1, "price" => 2.05, "expiration date" => "20.12.2018"],
-    [ "name" => "Консервы", "code" => "A02", "quantity" => 10, "price" => 1.05, "expiration date" => "20.12.2025"],
-    [ "name" => "Борщ", "code" => "A03", "quantity" => 1, "price" => 0.05, "expiration date" => "01.01.2007"],
+    ["name" => "Колбаса", "code" => "A01", "quantity" => 1, "price" => 2.05, "expiration date" => "20.12.2018"],
+    ["name" => "Консервы", "code" => "A02", "quantity" => 10, "price" => 1.05, "expiration date" => "20.12.2025"],
+    ["name" => "Борщ", "code" => "A03", "quantity" => 1, "price" => 0.05, "expiration date" => "01.01.2007"],
 ];
 
 foreach ($expDateItems as $key => $expDateItem) {
