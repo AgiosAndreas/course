bb0d3c0ded7e334777f5c046b4a74e2cb64a40b6
Space7Panda <space7panda@gmail.com>
2019-01-10 16:57:49 +0200

    5 done --kmplz

--- 08 PHP/homework/Space7Panda/4/5.sql
+++ 08 PHP/homework/Space7Panda/4/5.sql
@@ -1,1 +1,14 @@
 -- Сумма оплаченных покупок по каждой стране. Если валюта отличается от USD, то надо сконвертировать сумму в USD;
+SELECT 
+    customers.country,
+    SUM(
+        IF(customers.currency = 'USD', result.total_sum, result.total_sum * 0.87)
+    ) as total_sum_in_USD
+FROM customers
+    JOIN (
+        SELECT orders.customer_id, sum(orders.total) as total_sum
+        FROM orders
+        WHERE orders.paid = 1
+        GROUP BY orders.customer_id
+    ) as result ON customers.id = result.customer_id
+GROUP BY customers.country
