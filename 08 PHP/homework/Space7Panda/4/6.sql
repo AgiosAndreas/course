2c05ed325cca1b8e277ec2164cfabac68d19f732
space7panda <space7panda@gmail.com>
2019-01-12 12:56:48 +0200

    optimize mySql querry

--- 08 PHP/homework/Space7Panda/4/6.sql
+++ 08 PHP/homework/Space7Panda/4/6.sql
@@ -1,11 +1,8 @@
 -- Имя и id покупателя с самой большой суммой оплаченных покупок;
-SELECT customers.name, customers.id
+SELECT customers.name, customers.id, SUM(orders.total) as total_paid
 FROM customers
-WHERE customers.id = (
-    SELECT customer_id 
-    FROM orders 
-    WHERE orders.paid = 1
-    GROUP BY customer_id
-    ORDER BY SUM(total) DESC
-    LIMIT 1
-)
+LEFT JOIN orders ON customers.id = orders.customer_id
+WHERE orders.paid = 1
+GROUP BY customers.id
+ORDER BY SUM(orders.total) DESC
+LIMIT 1;
