8654dba4692ed208d02644ab8de2c0600383331e
Space7Panda <space7panda@gmail.com>
2019-01-14 15:00:48 +0200

    fix

--- 08 PHP/homework/Space7Panda/4/3.sql
+++ 08 PHP/homework/Space7Panda/4/3.sql
@@ -1,8 +1,5 @@
 -- Количество покупателей без оплаченных покупок;
-SELECT COUNT(*) as customers_with_debt FROM(
-    SELECT customers.name, orders.paid
+SELECT COUNT(DISTINCT customers.id)
     FROM customers
     LEFT JOIN orders ON customers.id = orders.customer_id 
     WHERE orders.paid = 0
-    GROUP BY customers.id
- ) as total;
