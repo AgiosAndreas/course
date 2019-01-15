710e9c97b7f20d2a13d17c3e826ed052a7dfd37c
space7panda <space7panda@gmail.com>
2019-01-15 10:42:04 +0200

    fix 6

--- 08 PHP/homework/Space7Panda/4/6.sql
+++ 08 PHP/homework/Space7Panda/4/6.sql
@@ -1,18 +1,16 @@
 -- Имя и id покупателя с самой большой суммой оплаченных покупок;
-SELECT * FROM(
-    SELECT 
-        customers.name,
-        customers.id,
-        SUM(
-            CASE
-            WHEN customers.currency = 'EUR' THEN orders.total * 0.87
-            ELSE orders.total
-            END
-        ) as total_paid_USD
-    FROM customers
-    LEFT JOIN orders ON customers.id = orders.customer_id
-    WHERE orders.paid = 1
-    GROUP BY customers.id
-) as new
-ORDER BY new.total_paid_USD DESC
+SELECT
+    customers.name,
+    customers.id,
+    SUM(
+        CASE
+        WHEN customers.currency = 'EUR' THEN orders.total * 0.87
+        ELSE orders.total
+        END
+    ) as total_USD
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id
+WHERE orders.paid = 1
+GROUP BY customers.id
+ORDER BY total_USD DESC
 LIMIT 1;
