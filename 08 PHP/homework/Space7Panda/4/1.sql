ed179e0d2eb24cfd23cc490f96337d03a2d612bd
space7panda <space7panda@gmail.com>
2019-01-15 13:03:36 +0200

    edit coding style

--- 08 PHP/homework/Space7Panda/4/1.sql
+++ 08 PHP/homework/Space7Panda/4/1.sql
@@ -1,7 +1,9 @@
 -- 5 самых больших покупок в евро(EUR);
-SELECT *
-FROM customers
-LEFT JOIN orders ON customers.id = orders.customer_id
-WHERE orders.paid = 1 AND customers.currency = 'EUR'
-ORDER BY orders.total DESC
-LIMIT 5;
+SELECT orders.id, orders.total as total_in_EUR
+  FROM customers
+    LEFT JOIN orders
+    ON customers.id = orders.customer_id
+  WHERE orders.paid = 1
+    AND customers.currency = 'EUR'
+  ORDER BY orders.total DESC
+  LIMIT 5;
--- 08 PHP/homework/Space7Panda/4/3.sql
+++ 08 PHP/homework/Space7Panda/4/3.sql
@@ -1,5 +1,7 @@
 -- Количество покупателей без оплаченных покупок;
 SELECT COUNT(DISTINCT customers.id)
-    FROM customers
-    LEFT JOIN orders ON customers.id = orders.customer_id 
-    WHERE orders.paid = 0 OR orders.paid IS NULL
+  FROM customers
+    LEFT JOIN orders
+    ON customers.id = orders.customer_id
+  WHERE orders.paid = 0
+    OR orders.paid IS NULL;
